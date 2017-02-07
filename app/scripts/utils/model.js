2463f15b1c581921f20a4a6c5bf5883c654099e6
Derek Lieu <dl@dereklieu.com>
2017-02-07 16:21:33 -0500

    Create utility to validate api responses

---
+++ app/scripts/utils/model.js
@@ -0,0 +1,18 @@
+'use strict';
+import assert from 'assert';
+import config from '../config';
+
+export const validate = function (model, object) {
+  // skip validation in staging, production environment
+  if (config.environment === 'development') {
+    return model(object);
+  }
+}
+
+export const collectionModel = function (c) {
+  assert.equal(typeof c.collectionName, 'string');
+}
+
+export const validateCollection = function () {
+  return (object) => validate(collectionModel, object);
+}
