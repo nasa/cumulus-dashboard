76616172b735775e193b63b015a4c311efac98c8
Derek Lieu <dl@dereklieu.com>
2017-02-07 18:04:39 -0500

    Include list collections action

---
+++ app/scripts/reducers/api.js
@@ -0,0 +1,38 @@
+'use strict';
+import { get, set } from 'object-path';
+
+import {
+  validateCollection
+} from '../utils/model';
+
+import * as ids from '../utils/ids';
+
+import {
+  AUTHENTICATED,
+  LIST_COLLECTIONS,
+  GET_COLLECTION
+} from '../actions';
+
+export const initialState = {
+  authenticated: true,
+  collections: [],
+  collectionDetail: {}
+};
+
+export default function reducer (state = initialState, action) {
+  state = Object.assign({}, state);
+  switch (action.type) {
+    case AUTHENTICATED:
+      set(state, 'authenticated', action.data);
+      break;
+    case LIST_COLLECTIONS:
+      action.data.forEach(validateCollection);
+      set(state, 'collections',  action.data);
+      break;
+    case GET_COLLECTION:
+      validateCollection(action.data);
+      set(state, ['collectionDetail', get(action.data, ids.collection)], action.data);
+      break;
+  }
+  return state;
+};
--- app/scripts/reducers/index.js
+++ app/scripts/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import api from './api';
+import errors from './errors';
 
 export const reducers = {
-  def: (state = {}, action) => state
+  def: (state = {}, action) => state,
+  api,
+  errors
 };
 
 export default combineReducers(Object.assign({}, reducers));
