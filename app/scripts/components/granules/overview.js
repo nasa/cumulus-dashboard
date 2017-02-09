6dcc1747c974654ec1fc684b02f481897b6b44e5
Derek Lieu <dl@dereklieu.com>
2017-02-09 17:56:31 -0500

    Light markup to components overview

--- app/scripts/components/granules/overview.js
+++ app/scripts/components/granules/overview.js
@@ -5,10 +5,22 @@ import { connect } from 'react-redux';
 var Overview = React.createClass({
   displayName: 'Overview',
 
+  propTypes: {
+    api: React.PropTypes.object,
+    dispatch: React.PropTypes.func
+  },
+
+  getProcessingGranules: function () {
+    // TODO hook up endpoint for processing granules once that's available
+  },
+
   render: function () {
     return (
       <div className='page__component'>
-        <h1>This is the overview for granules.</h1>
+        <h1 className='heading--large heading--shared-content'>Granules Overview</h1>
+        <div className='heading__wrapper--border'>
+          <h2 className='heading--medium'>Processing Granules (10)</h2>
+        </div>
       </div>
     );
   }
