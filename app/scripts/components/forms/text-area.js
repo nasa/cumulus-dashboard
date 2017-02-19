c040917909ead783fa62c910fe370080250e0177
Derek Lieu <dl@dereklieu.com>
2017-02-19 11:30:54 -0500

    Text area form component calls ace

--- app/scripts/components/forms/text-area.js
+++ app/scripts/components/forms/text-area.js
@@ -2,18 +2,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-var TextAreaForm = React.createClass({
+import 'brace';
+import 'brace/mode/json';
+import 'brace/theme/github';
+
+import Ace from 'react-ace';
+
+const tabSize = 2;
+const minLines = 12;
+const maxLines = 24;
+
+const defaultValues = {
+  json: '{}'
+};
+
+const TextAreaForm = React.createClass({
   displayName: 'TextAreaForm',
 
   propTypes: {
-    label: React.PropTypes.string
+    label: React.PropTypes.string,
+    id: React.PropTypes.string,
+    validate: React.PropTypes.func,
+    error: React.PropTypes.string,
+    mode: React.PropTypes.string
   },
 
   render: function () {
+    let {
+      label,
+      id,
+      // validate,
+      // error,
+      mode
+    } = this.props;
     return (
       <div className='form__textarea'>
-        <label>{this.props.label}</label>
-        <textarea rows="4" cols="50"></textarea>
+        <label>{label}</label>
+        <Ace
+          mode={mode}
+          theme='github'
+          onChange={this.onChange}
+          name={id}
+          value={defaultValues[mode]}
+
+          width='auto'
+          tabSize={tabSize}
+          showPrintMargin={false}
+          minLines={minLines}
+          maxLines={maxLines}
+          wrapEnabled={true}
+        />
       </div>
     );
   }
