3b06febc1de5fa7b626bec7ccdf3377d7988133c
Derek Lieu <dl@dereklieu.com>
2017-02-22 18:05:14 +0000

    Format function for comma-separted numbers

--- app/scripts/utils/format.js
+++ app/scripts/utils/format.js
@@ -1,9 +1,15 @@
 'use strict';
 import moment from 'moment';
+import numeral from 'numeral';
 
-const nullValue = '--';
+export const nullValue = '--';
 
 export const fullDate = function (datestring) {
   if (!datestring) { return nullValue; }
   return moment(datestring).format('MMM. Do, YYYY hh:mm:ss');
+};
+
+export const tally = function (numberString) {
+  if (!numberString || numberString === nullValue || isNaN(numberString)) { return nullValue; }
+  return numeral(+numberString).format('0,0');
 };
