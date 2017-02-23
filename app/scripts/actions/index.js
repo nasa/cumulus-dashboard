d9db0823bc84c15826d62887c9fc3d8986180b8b
Derek Lieu <dl@dereklieu.com>
2017-02-23 17:31:18 +0000

    Refactor actions with helper, add test

--- app/scripts/actions/index.js
+++ app/scripts/actions/index.js
@@ -1,106 +1,53 @@
 'use strict';
 import url from 'url';
-import request from 'request';
-import config from '../config';
+import { get, post, put, wrapRequest } from './helpers';
+import _config from '../config';
+const root = _config.apiRoot;
 
 export const ERROR = 'ERROR';
 export const AUTHENTICATED = 'AUTHENTICATED';
-
 export const QUERY_COLLECTION = 'QUERY_COLLECTION';
 export const GET_COLLECTION = 'GET_COLLECTION';
 export const LIST_COLLECTIONS = 'LIST_COLLECTIONS';
 export const POST_COLLECTION = 'POST_COLLECTION';
 export const PUT_COLLECTION = 'PUT_COLLECTION';
-
 export const LIST_GRANULES = 'LIST_GRANULES';
-
 export const GET_STATS = 'GET_STATS';
-
 export const LIST_PDRS = 'LIST_PDRS';
 
-const setError = (error) => ({ type: ERROR, data: error });
+export const setError = function (error) {
+  return { type: ERROR, data: error };
+};
+
 const queryCollection = (collectionName) => ({ type: QUERY_COLLECTION, data: { collectionName } });
 const setCollection = (collection) => ({ type: GET_COLLECTION, data: collection });
-const setCollections = (collections) => ({ type: LIST_COLLECTIONS, data: collections });
+const setCollections = (collections) => ({ type: LIST_COLLECTIONS, data: collections.results });
+const setPostCollection = (collection) => ({
+  type: POST_COLLECTION,
+  id: collection.collectionName,
+  data: collection
+});
+const setPutCollection = (collection) => ({
+  type: PUT_COLLECTION,
+  id: collection.collectionName,
+  data: collection
+});
 const setGranules = (granules) => ({ type: LIST_GRANULES, data: granules });
 const setStats = (stats) => ({ type: GET_STATS, data: stats });
 const setPdrs = (pdrs) => ({ type: LIST_PDRS, data: pdrs });
 
-export function setPostSuccess (type, post) {
-  return {
-    type,
-    data: post.data,
-    key: post.id,
-    postType: post.type
-  };
-}
-
-const root = config.apiRoot;
-function get (path, callback) {
-  request(url.resolve(root, path), (error, resp, body) => {
-    if (error) {
-      return callback(error);
-    }
-    try {
-      var data = JSON.parse(body);
-    } catch (e) {
-      return callback('JSON parse error');
-    }
-    return callback(null, data);
-  });
-}
-
-function post (path, payload, callback) {
-  request.post({
-    url: url.resolve(root, path),
-    body: payload,
-    json: true
-  }, (error, resp, body) => {
-    error = error || body.errorMessage;
-    if (error) {
-      return callback(error);
-    } else {
-      return callback(null, body);
-    }
-  });
-}
-
-function put (path, payload, callback) {
-  request.put({
-    url: url.resolve(root, path),
-    body: payload,
-    json: true
-  }, (error, resp, body) => {
-    error = error || body.errorMessage;
-    if (error) {
-      return callback(error);
-    } else {
-      return callback(null, body);
-    }
-  });
-}
-
-export function listCollections () {
-  return function (dispatch) {
-    get('collections', (error, data) => {
-      if (error) {
-        return dispatch(setError({
-          error,
-          meta: {
-            type: LIST_COLLECTIONS
-          }
-        }));
-      } else {
-        return dispatch(setCollections(data.results));
-      }
-    });
-  };
-}
+export const listCollections = () => wrapRequest(get, 'collections', LIST_COLLECTIONS, setCollections);
+export const createCollection = (payload) => wrapRequest(post, 'collections', payload, POST_COLLECTION, setPostCollection);
+export const updateCollection = (payload) => wrapRequest(put, 'collections', payload, PUT_COLLECTION, setPutCollection);
+export const listGranules = () => wrapRequest(get, 'granules', LIST_GRANULES, setGranules);
+export const getStats = () => wrapRequest(get, 'stats/summary/grouped', GET_STATS, setStats);
+export const listPdrs = () => wrapRequest(get, 'pdrs', LIST_PDRS, setPdrs);
 
 export function getCollection (collectionName) {
   return function (dispatch) {
     dispatch(queryCollection(collectionName));
-    get(`collections?collectionName=${collectionName}`, (error, data) => {
+    let path = url.resolve(root, `collections?collectionName=${collectionName}`);
+    get(path, (error, data) => {
       if (error) {
         return dispatch(setError({
           error,
@@ -112,100 +59,6 @@ export function getCollection (collectionName) {
         }));
       } else {
         return dispatch(setCollection(data.results[0]));
-      }
-    });
-  };
-}
-
-export function createCollection (payload) {
-  return function (dispatch) {
-    post('collections', payload, (error, data) => {
-      if (error) {
-        return dispatch(setError({
-          error,
-          meta: {
-            type: POST_COLLECTION,
-            id: payload.collectionName
-          }
-        }));
-      } else {
-        return dispatch(setPostSuccess(POST_COLLECTION, {
-          type: 'collection',
-          id: data.collectionName,
-          data
-        }));
-      }
-    });
-  };
-}
-
-export function updateCollection (payload) {
-  return function (dispatch) {
-    put('collections', payload, (error, data) => {
-      if (error) {
-        return dispatch(setError({
-          error,
-          meta: {
-            type: PUT_COLLECTION,
-            id: payload.collectionName
-          }
-        }));
-      } else {
-        return dispatch(setPostSuccess(PUT_COLLECTION, {
-          type: 'collection',
-          id: data.Attributes.collectionName,
-          data
-        }));
-      }
-    });
-  };
-}
-
-export function listGranules () {
-  return function (dispatch) {
-    get('granules', (error, data) => {
-      if (error) {
-        return dispatch(setError({
-          error,
-          meta: {
-            type: LIST_GRANULES
-          }
-        }));
-      } else {
-        return dispatch(setGranules(data));
-      }
-    });
-  };
-}
-
-export function getStats () {
-  return function (dispatch) {
-    get('stats/summary/grouped', (error, data) => {
-      if (error) {
-        return dispatch(setError({
-          error,
-          meta: {
-            type: GET_STATS
-          }
-        }));
-      } else {
-        return dispatch(setStats(data));
-      }
-    });
-  };
-}
-
-export function listPdrs () {
-  return function (dispatch) {
-    get('pdrs', (error, data) => {
-      console.log(data);
-      if (error) {
-        return dispatch(setError({
-          error,
-          meta: { type: LIST_PDRS }
-        }));
-      } else {
-        return dispatch(setPdrs(data));
       }
     });
   };
