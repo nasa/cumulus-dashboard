39c1f35a2e7018194c3ddc37ffbab38eab5750d7
Derek Lieu <dl@dereklieu.com>
2017-02-24 14:39:06 +0000

    Selectively choose route set, replace variables

--- app/scripts/paths/collections.js
+++ app/scripts/paths/collections.js
@@ -1,9 +1,10 @@
 'use strict';
+import { encode } from '../utils/browser';
 
 const singleCollectionRoutes = [
   ['Back to Collections', null, 'sidebar__nav--back'],
-  ['Active', 'active'],
-  ['In-active', 'inactive']
+  ['Active', 'collection/:collectionName/active'],
+  ['In-active', 'collection/:collectionName/inactive']
 ];
 
 const collectionRoutes = [
@@ -17,7 +18,17 @@ const collectionRoutes = [
 const collections = {
   base: 'collections',
   heading: 'Collections',
-  routes: (params) => {
+  routes: (currentRoute, params) => {
+    // determine which set of routes to show, based on the current route
+    if (currentRoute.indexOf('collections/collection') >= 0) {
+      return singleCollectionRoutes.map(d => {
+        // replace wildecards with params
+        if (!d[1] || d[1].indexOf(':collectionName') === -1) { return d; }
+        let copy = d.slice();
+        copy[1] = encode(copy[1].replace(':collectionName', params.collectionName));
+        return copy;
+      });
+    }
     return collectionRoutes;
   }
 };
