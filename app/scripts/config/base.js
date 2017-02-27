88f6e71a035aaf4d11ce6db7912f5024e075a60d
Derek Lieu <dl@dereklieu.com>
2017-02-27 16:36:00 +0000

    Shorten page limit for now

--- app/scripts/config/base.js
+++ app/scripts/config/base.js
@@ -2,13 +2,10 @@ module.exports = {
   environment: 'development',
   apiRoot: 'https://0uqm56f3j2.execute-api.us-east-1.amazonaws.com/dev/',
 
-  // api list query params
-  pageSize: 20,
-
   // settings for Ace editor
   editorTheme: 'github',
   tabSize: 2,
 
   // settings for list queries
-  pageLimit: 100
+  pageLimit: 20
 };
