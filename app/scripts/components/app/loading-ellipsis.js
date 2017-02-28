ddd590c5221a241234af56674828d55710713fb3
Derek Lieu <dl@dereklieu.com>
2017-02-28 18:29:59 +0000

    Fix classname

--- app/scripts/components/app/loading-ellipsis.js
+++ app/scripts/components/app/loading-ellipsis.js
@@ -4,10 +4,10 @@ const Loading = React.createClass({
   displayName: 'Ellipsis',
   render: function () {
     return (
-      <div class="spinner">
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
-        <div class="bounce3"></div>
+      <div className="spinner">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
       </div>
     );
   }
