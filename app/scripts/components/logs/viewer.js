fb1adf0d6bfc135494aa27327255f6cbc2c8814b
Derek Lieu <dl@dereklieu.com>
2017-03-01 16:52:05 +0000

    Build log viewer component

---
+++ app/scripts/components/logs/viewer.js
@@ -0,0 +1,65 @@
+'use strict';
+import React from 'react';
+import { interval, getLogs } from '../../actions';
+import { logsUpdateInterval } from '../../config';
+import moment from 'moment';
+
+var LogViewer = React.createClass({
+  displayName: 'LogViewer',
+  propTypes: {
+    dispatch: React.PropTypes.func,
+    query: React.PropTypes.string,
+    logs: React.PropTypes.object
+  },
+
+  componentWillMount: function () {
+    this.query();
+  },
+
+  componentWillReceiveProps: function (newProps) {
+    if (newProps.query !== this.props.query) {
+      const query = newProps.query || 'none';
+      this.query(query);
+    }
+  },
+
+  componentWillUnmount: function () {
+    if (this.cancelInterval) { this.cancelInterval(); }
+  },
+
+  query: function (query) {
+    // note, since the non-filtered endpoint is very slow,
+    // enforce a filter or none.
+    query = query || this.props.query || 'none';
+    const { dispatch } = this.props;
+    if (this.cancelInterval) { this.cancelInterval(); }
+
+    let isFirstPull = true;
+    function querySinceLast () {
+      // on first pull, get the last 24 hours
+      const duration = isFirstPull ? 1000 * 60 * 60 * 24 : logsUpdateInterval;
+      const from = moment().subtract(duration, 'milliseconds').format();
+      isFirstPull = false;
+      return dispatch(getLogs({
+        q: query,
+        date_from: from
+      }));
+    }
+    this.cancelInterval = interval(querySinceLast, logsUpdateInterval, true);
+  },
+
+  render: function () {
+    return (
+      <div className='logs'>
+        {this.props.logs.items.map((d, i) => {
+          const key = d.timestamp + d.data;
+          return <p
+            key={key}
+            className='logs__item'><span className='logs__item--date'>{d.displayTime}</span> <span className={'logs__item--level logs__item--' + d.level}>{d.level}</span> {d.displayText}</p>;
+        })}
+      </div>
+    );
+  }
+});
+
+export default LogViewer;
