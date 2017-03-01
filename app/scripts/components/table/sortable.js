bbe9c1514de520121f8dd59a1b5fdaeab7e51a1c
Derek Lieu <dl@dereklieu.com>
2017-03-01 19:56:54 +0000

    Table takes sort properties

--- app/scripts/components/table/sortable.js
+++ app/scripts/components/table/sortable.js
@@ -1,25 +1,49 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
+import { isUndefined } from '../../utils/validate';
+
+const defaultSortOrder = 'desc';
+const otherOrder = {
+  desc: 'asc',
+  asc: 'desc'
+};
 
 const Table = React.createClass({
   displayName: 'SortableTable',
 
   propTypes: {
+    primaryIdx: React.PropTypes.number,
     data: React.PropTypes.array,
     header: React.PropTypes.array,
     row: React.PropTypes.array,
-    primaryItemIndex: React.PropTypes.number
+    sortIdx: React.PropTypes.number,
+    order: React.PropTypes.string,
+    changeSortProps: React.PropTypes.func
+  },
+
+  changeSort: function (e) {
+    if (isUndefined(this.props.sortIdx) || isUndefined(this.props.order)) { return; }
+    const value = e.currentTarget.getAttribute('data-value');
+    const sortIdx = this.props.header.indexOf(value);
+    const order = this.props.sortIdx === sortIdx ? otherOrder[this.props.order] : defaultSortOrder;
+    if (typeof this.props.changeSortProps === 'function') {
+      this.props.changeSortProps({ sortIdx, order });
+    }
   },
 
   render: function () {
-    const { row, data } = this.props;
-    const pIndex = this.props.primaryItemIndex || 0;
+    let { primaryIdx, sortIdx, order, row, data } = this.props;
+    primaryIdx = primaryIdx || 0;
     return (
       <table>
         <thead>
           <tr>
-            {this.props.header.map(h => <td key={h}>{h}</td>)}
+            {this.props.header.map((h, i) => <td
+              className={i === sortIdx ? 'table__sort table__sort--' + order : ''}
+              key={h}
+              data-value={h}
+              onClick={this.changeSort}>{h}</td>)}
           </tr>
         </thead>
         <tbody>
@@ -27,7 +51,7 @@ const Table = React.createClass({
             return (
               <tr key={i}>
                 {row.map((accessor, k) => {
-                  let className = k === pIndex ? 'table__main-asset' : '';
+                  let className = k === primaryIdx ? 'table__main-asset' : '';
                   let text;
                   if (typeof accessor === 'function') {
                     text = accessor(d, k, data);
