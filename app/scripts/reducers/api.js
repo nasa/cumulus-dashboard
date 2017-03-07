171497a4705f3d36b9911ac6200b52b095861a92
Derek Lieu <dl@dereklieu.com>
2017-03-07 15:59:38 +0000

    Set login error, inflight actions

--- app/scripts/reducers/api.js
+++ app/scripts/reducers/api.js
@@ -2,19 +2,36 @@
 import { set } from 'object-path';
 
 import {
-  AUTHENTICATED
+  LOGIN,
+  LOGIN_INFLIGHT,
+  LOGIN_ERROR,
+  LOGOUT
 } from '../actions';
 
 export const initialState = {
-  authenticated: true
+  authenticated: false,
+  inflight: false,
+  error: null
 };
 
 export default function reducer (state = initialState, action) {
   state = Object.assign({}, state);
   switch (action.type) {
-    case AUTHENTICATED:
-      set(state, 'authenticated', action.data);
+    case LOGIN:
+      set(state, 'authenticated', true);
+      set(state, 'inflight', false);
       break;
+    case LOGIN_INFLIGHT:
+      set(state, 'inflight', true);
+      break;
+    case LOGIN_INFLIGHT:
+      set(state, 'error', action.error);
+      set(state, 'inflight', false);
+      break;
+    case LOGOUT:
+      set(state, 'authenticated', false);
+      break;
+
   }
   return state;
 }
