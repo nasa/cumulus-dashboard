d0689739183cebfd5d8fcf3079d0e0152da003c6
Derek Lieu <dl@dereklieu.com>
2017-03-07 17:01:26 +0000

    App header controls login modal

--- app/scripts/components/app/header.js
+++ app/scripts/components/app/header.js
@@ -1,12 +1,67 @@
 'use strict';
 import React from 'react';
 import { Link } from 'react-router';
+import { login, logout } from '../../actions';
 import { graphicsPath } from '../../config';
+import { Form, formTypes } from '../form';
+import * as validate from '../../utils/validate';
+
+const inputElements = [
+  {
+    schemaProperty: 'user',
+    label: 'Username',
+    type: formTypes.text,
+    validate: validate.isText
+  },
+  {
+    schemaProperty: 'pass',
+    label: 'Password',
+    type: formTypes.text,
+    validate: validate.isText
+  }
+];
 
 var Header = React.createClass({
   displayName: 'Header',
+  propTypes: {
+    dispatch: React.PropTypes.func,
+    api: React.PropTypes.object
+  },
+
+  getInitialState: function () {
+    return {
+      showModal: false
+    };
+  },
+
+  componentWillReceiveProps: function (newProps) {
+    // delay-close the modal if it's open
+    if (newProps.api.authenticated && this.state.showModal) {
+      setTimeout(this.hideLoginModal, 1000);
+    }
+  },
+
+  showLoginModal: function () {
+    this.setState({ showModal: true });
+  },
+
+  hideLoginModal: function () {
+    this.setState({ showModal: false });
+  },
+
+  login: function (credentials) {
+    const { user, pass } = credentials;
+    const token = new Buffer(`${user}:${pass}`).toString('base64');
+    this.props.dispatch(login(token));
+  },
+
+  logout: function () {
+    this.props.dispatch(logout());
+  },
 
   render: function () {
+    const { authenticated, inflight } = this.props.api;
+    const { showModal } = this.state;
     return (
       <div className='header'>
         <div className='row'>
@@ -18,8 +73,25 @@ var Header = React.createClass({
               <li><Link to='/pdrs'>PDR's</Link></li>
               <li><Link to='/logs'>Logs</Link></li>
               <li><Link to='/contact'>Contact</Link></li>
+              <li className='rightalign'>{ authenticated ? <a onClick={this.logout}>Log out</a> : <a onClick={this.showLoginModal}>Log in</a> }</li>
             </ul>
           </nav>
+        </div>
+
+        { showModal ?  <div className='modal__cover'></div> : null }
+
+        <div className={ showModal ? 'login login__onscreen' : 'login' }>
+          <div className='modal'>
+            <div className='modal__internal'>
+              <p>{ authenticated ? 'Success!' : 'Enter your username and password' }</p>
+              <Form
+                inputMeta={inputElements}
+                cancel={this.hideLoginModal}
+                submit={this.login}
+                inflight={inflight}
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
