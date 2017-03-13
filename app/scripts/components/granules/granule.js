b0d72ca7905ff1f771f9b19713d2919239c4b0b9
Derek Lieu <dl@dereklieu.com>
2017-03-13 14:53:09 +0000

    Introduce more data to granule page

--- app/scripts/components/granules/granule.js
+++ app/scripts/components/granules/granule.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { interval, getGranule, reprocessGranule } from '../../actions';
 import { get } from 'object-path';
-import { fullDate, lastUpdated, nullValue } from '../../utils/format';
+import { fullDate, lastUpdated, seconds, nullValue } from '../../utils/format';
 import SortableTable from '../table/sortable';
 import Loading from '../app/loading-indicator';
 import Ellipsis from '../app/loading-ellipsis';
@@ -26,6 +26,29 @@ const tableRow = [
   (d) => (<a href={d.stagingFile}>{d.stagingFile ? link : nullValue}</a>),
   (d) => (<a href={d.archivedFile}>{d.archivedFile ? link : nullValue}</a>),
   (d) => d.access
+];
+
+const metaAccessors = [
+  ['Created', 'createdAt', fullDate],
+  ['Last updated', 'updatedAt', fullDate],
+
+  ['Ingested started', 'ingestStartedAt', fullDate],
+  ['Ingest ended', 'ingestEndedAt', fullDate],
+
+  ['CMR push started', 'timeline.pushToCMR.startedAt', fullDate],
+  ['CMR push ended', 'timeline.pushToCMR.endedAt', fullDate],
+
+  ['Archive started', 'timeline.archive.startedAt', fullDate],
+  ['Archive ended', 'timeline.archive.endedAt', fullDate],
+
+  ['Processing started', 'timeline.processStep.startedAt', fullDate],
+  ['Processing ended', 'timeline.processStep.endedAt', fullDate],
+
+  ['Ingest duration', 'ingestDuration', seconds],
+  ['CMR Duration', 'pushToCMRDuration', seconds],
+  ['Archive duration', 'archiveDuration', seconds],
+  ['Processing duration', 'processingDuration', seconds],
+  ['Total duration', 'totalDuration', seconds]
 ];
 
 var GranuleOverview = React.createClass({
@@ -112,9 +135,7 @@ var GranuleOverview = React.createClass({
     const granuleId = this.props.params.granuleId;
     const record = get(this.props.granules.map, granuleId);
 
-    if (!record) {
-      return <div></div>;
-    } else if (record.inflight && !record.data) {
+    if (record.inflight && !record.data) {
       return <Loading />;
     }
 
@@ -126,6 +147,7 @@ var GranuleOverview = React.createClass({
       for (let key in granule.files) { files.push(granule.files[key]); }
     }
     const logsQuery = { granuleId };
+    const cmrLink = get(granule, 'cmrLink');
     return (
       <div className='page__component'>
         <section className='page__section'>
@@ -141,13 +163,19 @@ var GranuleOverview = React.createClass({
 
         <section className='page__section'>
           <div className='heading__wrapper--border'>
-            <h2 className='heading--medium'>Granule Overview</h2>
+            <h2 className='heading--medium'>Granule Overview {cmrLink ? <a href={cmrLink}>[CMR]</a> : null}</h2>
           </div>
           <dl className='metadata__granule__details'>
-            <dt>Created</dt>
-            <dd>{fullDate(granule.createdAt)}</dd>
-            <dt>Ingested</dt>
-            <dd>{fullDate(granule.ingestEnded)}</dd>
+            {metaAccessors.reduce((acc, meta, i) => {
+              let value = get(granule, meta[1]);
+              if (value !== nullValue && typeof meta[2] === 'function') {
+                value = meta[2](value);
+              }
+              return acc.concat([
+                <dt key={`meta-${meta[1]}--dt`}>{meta[0]}</dt>,
+                <dd key={`meta-${meta[1]}--dd`}>{value}</dd>
+              ]);
+            }, [])}
           </dl>
         </section>
 
