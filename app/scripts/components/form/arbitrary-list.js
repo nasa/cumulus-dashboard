102d74cdc9fccfb9699d833f104f7c5bfaa391a1
Derek Lieu <dl@dereklieu.com>
2017-03-16 17:04:21 +0100

    Only allow adds when there's something in the last field

--- app/scripts/components/form/arbitrary-list.js
+++ app/scripts/components/form/arbitrary-list.js
@@ -19,12 +19,14 @@ var List = React.createClass({
   add: function (e) {
     e.preventDefault();
     const value = this.props.value.slice();
+    if (!value[value.length - 1]) return false;
     value.push('');
     this.props.onChange(this.props.id, value);
   },
 
   remove: function (index) {
-    const value = this.props.value.slice().splice(index);
+    let value = this.props.value.slice()
+    value.splice(index, 1);
     this.props.onChange(this.props.id, value);
   },
 
@@ -50,14 +52,23 @@ var List = React.createClass({
 
   renderItem: function (item, i, items) {
     const add = i === items.length - 1;
+    // only allow adds if you've entered something
+    const disabled = add && !item.length;
     return (
-      <li key={item + i} className='form__list--item'>
+      <li key={i} className='form__list--item'>
         <input
           value={item}
           onChange={(e) => this.onChange(i, e.target.value)}
         />
-        { add ? <button onClick={this.add} className='button form__list--button'>+</button> : null }
-        { !add ? <button onClick={(e) => { e.preventDefault(); this.remove(i); }} className='button form__list--button'>-</button> : null }
+        { add ? <button
+          onClick={this.add}
+          className={'button form__list--button' + (disabled ? ' button--disabled': '')}>+</button>
+        : null }
+
+        { !add ? <button
+          onClick={(e) => { e.preventDefault(); this.remove(i); }}
+          className='button form__list--button'>-</button>
+        : null }
       </li>
     );
   }
