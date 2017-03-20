880853fc888d8ee5adb5388f47c6784e429017ca
Derek Lieu <dl@dereklieu.com>
2017-03-20 09:55:03 +0000

    Break out traversal and form creation, support pattern properties

--- app/scripts/components/form/schema.js
+++ app/scripts/components/form/schema.js
@@ -56,7 +56,7 @@ export const Schema = React.createClass({
   traverseSchema: function (schema, fn, path) {
     for (let property in schema.properties) {
       const meta = schema.properties[property];
-      if (meta.type === 'object') {
+      if (meta.type === 'object' && meta.hasOwnProperty('properties')) {
         const nextPath = path ? path + '.' + property : property;
         this.traverseSchema(meta, fn, nextPath);
       } else {
@@ -65,21 +65,51 @@ export const Schema = React.createClass({
     }
   },
 
+  // recursively scan a schema object and create a form config from it.
+  // returns a flattened representation of the schema.
   createFormConfig: function (data, schema) {
+    data = data || {};
     const fields = [];
     this.traverseSchema(schema, function (property, meta, schemaProperty, path) {
+      // determine the label
       const required = Array.isArray(schemaProperty.required) &&
         schemaProperty.required.indexOf(property) >= 0;
       var label = meta.title || property;
       if (meta.description) label += ` (${meta.description})`;
       if (required) label += ' *required';
 
+      // create an object-path-ready accessor string
       const accessor = path ? path + '.' + property : property;
-      const value = get(data, accessor);
+      const value = get(data, accessor) || get(meta, 'default');
       const config = { value, label, schemaProperty: accessor };
 
-      const type = Array.isArray(meta['enum']) ? 'enum' : meta.type;
+      // dropdowns have type set to string, but have an enum prop.
+      // use enum as the type instead of string.
+      const type = Array.isArray(meta['enum']) ? 'enum'
+        : meta.hasOwnProperty('patternProperties') ? 'pattern' : meta.type;
       switch (type) {
+        case 'pattern':
+          // pattern fields are an abstraction on arrays of objects.
+          // each item in the array will be a grouped set of field inputs.
+
+          // get any pre-existing objects, assigning the object key as _key.
+          // this becomes the nested object's "name".
+          var values = value ? Object.keys(value).map(key => Object.assign({_key: key}, value[key])) : [{}];
+          if (!values.length) values = [{}];
+
+          // get the first pattern property, which should be a regex that
+          // validates _key.
+          const { patternProperties } = meta;
+          const pattern = Object.keys(patternProperties)[0];
+
+          // attach the fields
+          config.fields = values.map(item => ({
+            name: item._key,
+            forms: this.createFormConfig(item, patternProperties[pattern])
+          }));
+          config.type = formTypes.subform;
+          fields.push(config);
+          break;
         case 'enum':
           // pass the enum fields as options
           config.options = meta.enum;
@@ -101,63 +131,6 @@ export const Schema = React.createClass({
         default: return;
       }
     }.bind(this));
-    return fields;
-  },
-
-
-  // recursively scan a schema object and create a form config from it.
-  // returns a flattened representation of the schema.
-  traverseObject: function (data, object, path) {
-    let requiredProperties = object.required || [];
-    let fields = [];
-
-    // traverse each property in this object
-    for (let property in object.properties) {
-      // the schema field
-      const schema = object.properties[property];
-
-      // dropdowns have type set to string, but have an enum prop.
-      // use enum as the type instead of string.
-      const type = Array.isArray(schema['enum']) ? 'enum' : schema.type;
-
-      // create an object-path-ready accessor string
-      const accessor = path ? path + '.' + property : property;
-      if (type === 'object') {
-        // recursively traverse if it's another object
-        fields = fields.concat(this.traverseObject(data, schema, accessor));
-      } else {
-        // determine the label
-        var label = schema.title || property;
-        const required = requiredProperties.indexOf(property) >= 0;
-        if (schema.description) label += ` (${schema.description})`;
-        if (required) label += ' *required';
-
-        // create the form configuration
-        const value = get(data, accessor);
-        var config = { value, label, schemaProperty: accessor };
-        switch (type) {
-          case 'enum':
-            // pass the enum fields as options
-            config.options = schema.enum;
-            fields.push(this.dropdown(config, property, (required && isText)));
-            break;
-          case 'array':
-            // some array types have a minItems property
-            let validate = !required ? null
-              : (schema.minItems && isNaN(schema.minItems))
-                ? arrayWithLength(+schema.minItems) : isArray;
-            fields.push(this.list(config, property, validate));
-            break;
-          case 'string':
-            fields.push(this.textfield(config, property, (required && isText)));
-            break;
-          case 'number':
-            fields.push(this.textfield(config, property, (required && isNumber)));
-            break;
-          default: continue;
-        }
-      }
-    }
     return fields;
   },
 
