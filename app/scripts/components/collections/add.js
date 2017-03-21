0f704fdf6690bcd925197db6efa92482eac60fee
Derek Lieu <dl@dereklieu.com>
2017-03-21 15:29:49 +0000

    Add schema format to new collection page

--- app/scripts/components/collections/add.js
+++ app/scripts/components/collections/add.js
@@ -2,50 +2,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { get } from 'object-path';
-import { Form, formTypes, defaults } from '../form';
-import { createCollection } from '../../actions';
-import * as validate from '../../utils/validate';
-import t from '../../utils/strings';
+import { getSchema, createCollection } from '../../actions';
 import ErrorReport from '../errors/report';
+import Schema from '../form/schema';
 import Loading from '../app/loading-indicator';
 
-const inputElements = [
-  {
-    schemaProperty: 'collectionName',
-    label: 'Collection Name',
-    type: formTypes.text,
-    validate: validate.isText,
-    error: t.errors.collectionName
-  },
-
-  {
-    schemaProperty: 'granuleDefinition',
-    label: 'Granule Definition',
-    type: formTypes.textArea,
-    mode: 'json',
-    value: defaults.json
-  },
-
-  {
-    schemaProperty: 'ingest',
-    label: 'Ingest',
-    type: formTypes.textArea,
-    mode: 'json',
-    value: defaults.json
-  },
-
-  {
-    schemaProperty: 'recipe',
-    label: 'Recipe',
-    type: formTypes.textArea,
-    mode: 'json',
-    value: defaults.json
-  }
-];
+const SCHEMA_KEY = 'collection';
 
 var AddCollection = React.createClass({
-  displayName: 'AddCollection',
-
   getInitialState: function () {
     return {
       collectionName: null
@@ -54,7 +18,12 @@ var AddCollection = React.createClass({
 
   propTypes: {
     dispatch: React.PropTypes.func,
-    collections: React.PropTypes.object
+    collections: React.PropTypes.object,
+    schema: React.PropTypes.object
+  },
+
+  componentWillMount: function () {
+    this.props.dispatch(getSchema(SCHEMA_KEY));
   },
 
   post: function (id, payload) {
@@ -73,18 +42,16 @@ var AddCollection = React.createClass({
     const { collectionName } = this.state;
     const record = collectionName
       ? get(this.props.collections.created, collectionName, {}) : {};
+    const schema = this.props.schema[SCHEMA_KEY];
     return (
       <div className='page__component'>
         <section className='page__section'>
           <div className='page__section__header'>
             <h1 className='heading--large'>Add a Collection</h1>
-            <p className='description'>Instructions to add JSON in the below fields. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt, orci vel tincidunt ultricies, augue libero egestas felis, vel blandit arcu elit et nisl. Pellentesque luctus sapien eu augue sodales auctor.</p>
+            <p className='description'>Create a collection</p>
           </div>
+          {schema ? <Schema schema={schema} pk={'new-collection'} onSubmit={this.post} /> : null}
           {record.status === 'inflight' ? <Loading /> : null}
-          <Form
-            inputMeta={inputElements}
-            submit={this.post}
-          />
           {record.status === 'error' ? <ErrorReport report={record.error} /> : null}
         </section>
       </div>
