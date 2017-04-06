c9e72c84aacfd13a72c1cbaec6bdf44a5ba63686
Derek Lieu <dl@dereklieu.com>
2017-04-06 12:27:39 +0100

    Create a loading indicator in the confirmation modal

--- app/scripts/components/form/batch-async-command.js
+++ app/scripts/components/form/batch-async-command.js
@@ -2,8 +2,10 @@
 import React from 'react';
 import { queue } from 'd3-queue';
 import AsyncCommand from './async-command';
+import { updateDelay } from '../../config';
 
 const CONCURRENCY = 3;
+const IN_PROGRESS = 'Processing...';
 
 const BatchCommand = React.createClass({
   propTypes: {
@@ -21,13 +23,14 @@ const BatchCommand = React.createClass({
   getInitialState: function () {
     return {
       callbacks: {},
-      activeModal: false
+      activeModal: false,
+      completed: 0
     };
   },
 
   componentWillReceiveProps: function (newProps) {
     const { state } = newProps;
-    const { callbacks } = this.state;
+    const { callbacks, completed } = this.state;
     // on success or error, call and remove the saved callback
     Object.keys(callbacks).forEach(id => {
       if (!state[id] || !callbacks[id]) return;
@@ -35,9 +38,30 @@ const BatchCommand = React.createClass({
       else if (state[id].status === 'error') callbacks[id](state[id].error);
       if (state[id].status === 'success' || state[id].status === 'error') {
         delete callbacks[id];
-        this.setState({ callbacks });
+        this.setState({ callbacks, completed: completed + 1 });
       }
     });
+  },
+
+  confirm: function () {
+    if (!this.isInflight()) this.start();
+  },
+
+  cancel: function () {
+    // prevent cancel when inflight
+    if (!this.isInflight()) this.setState({ activeModal: false });
+  },
+
+  start: function () {
+    const { selection } = this.props;
+    // if we have inflight callbacks, don't allow further clicks
+    if (!Array.isArray(selection) || !selection.length ||
+      this.isInflight()) return false;
+    const q = queue(CONCURRENCY);
+    for (let i = 0; i < selection.length; ++i) {
+      q.defer(this.initAction, selection[i]);
+    }
+    q.awaitAll(this.onComplete);
   },
 
   // save a reference to the callback in state, then init the action
@@ -54,6 +78,11 @@ const BatchCommand = React.createClass({
     const { onSuccess, onError } = this.props;
     if (error && typeof onError === 'function') onError(error);
     else if (!error && typeof onSuccess === 'function') onSuccess(results);
+    setTimeout(() => this.cleanup(), updateDelay);
+  },
+
+  cleanup: function () {
+    this.setState({ activeModal: false, completed: 0 });
   },
 
   isInflight: function () {
@@ -66,49 +95,38 @@ const BatchCommand = React.createClass({
     } else this.start();
   },
 
-  confirm: function () {
-
-  },
-
-  cancel: function () {
-
-  },
-
-  start: function () {
-    const { selection } = this.props;
-    // if we have inflight callbacks, don't allow further clicks
-    if (!Array.isArray(selection) || !selection.length ||
-      this.isInflight()) return false;
-    const q = queue(CONCURRENCY);
-    for (let i = 0; i < selection.length; ++i) {
-      q.defer(this.initAction, selection[i]);
-    }
-    q.awaitAll(this.onComplete);
-  },
-
   render: function () {
     const { text, selection, className, confirm } = this.props;
+    const { activeModal, completed } = this.state;
+    const todo = selection.length;
+    const done = (todo && completed === todo);
     const inflight = this.isInflight();
-    const { activeModal } = this.state;
     return (
       <div>
         <AsyncCommand
           action={this.handleClick}
           text={text}
           className={className}
-          disabled={!selection.length || inflight}
-          status={inflight ? 'inflight' : null}
+          disabled={!activeModal && (!todo || !!inflight)}
+          status={!activeModal && inflight ? 'inflight' : null}
         />
         { activeModal ? <div className='modal__cover'></div> : null }
         <div className={ activeModal ? 'modal__container modal__container--onscreen' : 'modal__container' }>
           { activeModal ? (
             <div className='modal'>
               <div className='modal__internal modal__formcenter'>
-                <h4>{confirm(selection.length)}</h4>
-                <button className='button button__animation--md button__arrow button__arrow--md button__animation button__arrow--white'
+                <h4>{done ? 'Success!' : inflight ? IN_PROGRESS : confirm(todo)}</h4>
+                <button className={'button button__animation--md button__arrow button__arrow--md button__animation button__arrow--white' + (inflight || done ? ' button--disabled' : '')}
                   onClick={this.confirm}>Confirm</button>
-                <button className='button button__animation--md button__arrow button__arrow--md button__animation button--secondary form-group__element--left button__cancel'
+                <button className={'button button__animation--md button__arrow button__arrow--md button__animation button--secondary form-group__element--left button__cancel' + (inflight || done ? ' button--disabled' : '')}
                   onClick={this.cancel}>Cancel</button>
+                <div className='modal__loading'>
+                  <div className='modal__loading--inner'>
+                    <div className='modal__loading--progress'
+                      style={{width: (todo ? (completed * 100 / todo) + '%' : 0)}}>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           ) : null}
