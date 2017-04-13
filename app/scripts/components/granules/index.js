a9b467d31f4e526deaeb2259e4beba1867e110aa
Derek Lieu <dl@dereklieu.com>
2017-04-13 18:56:12 +0100

    Include record count in granules sidebar

--- app/scripts/components/granules/index.js
+++ app/scripts/components/granules/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { get } from 'object-path';
 import Sidebar from '../app/sidebar';
+import { interval, getCount } from '../../actions';
+import { updateInterval } from '../../config';
 
 var Granules = React.createClass({
   displayName: 'Granules',
@@ -8,10 +11,28 @@ var Granules = React.createClass({
   propTypes: {
     children: React.PropTypes.object,
     location: React.PropTypes.object,
-    params: React.PropTypes.object
+    params: React.PropTypes.object,
+    dispatch: React.PropTypes.func,
+    stats: React.PropTypes.object
+  },
+
+  componentWillMount: function () {
+    this.cancelInterval = interval(() => this.query(), updateInterval, true);
+  },
+
+  componentWillUnmount: function () {
+    if (this.cancelInterval) { this.cancelInterval(); }
+  },
+
+  query: function () {
+    this.props.dispatch(getCount({
+      type: 'granules',
+      field: 'status'
+    }));
   },
 
   render: function () {
+    const count = get(this.props.stats, 'count.data.granules.count');
     return (
       <div className='page__granules'>
         <div className='content__header'>
@@ -21,7 +42,11 @@ var Granules = React.createClass({
         </div>
         <div className='page__content'>
           <div className='row wrapper__sidebar'>
-            <Sidebar currentPath={this.props.location.pathname} params={this.props.params} />
+            <Sidebar
+              currentPath={this.props.location.pathname}
+              params={this.props.params}
+              count={count}
+            />
             <div className='page__content--shortened'>
               {this.props.children}
             </div>
