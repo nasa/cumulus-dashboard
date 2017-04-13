f7697cdce326edf47247e92a1fd97e9193676068
Derek Lieu <dl@dereklieu.com>
2017-04-13 17:21:15 +0100

    Close #351 fix granule ingest, recipe page

--- app/scripts/components/granules/recipe-ingest.js
+++ app/scripts/components/granules/recipe-ingest.js
@@ -1,21 +1,29 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
-import { getGranule } from '../../actions';
+import {
+  getGranule,
+  reprocessGranule,
+  reingestGranule,
+  removeGranule,
+  deleteGranule
+} from '../../actions';
 import { get } from 'object-path';
-import { Link } from 'react-router';
 import { lastUpdated, seconds } from '../../utils/format';
 import Ace from 'react-ace';
 import config from '../../config';
 import Loading from '../app/loading-indicator';
+import AsyncCommands from '../form/dropdown-async-command';
 
+const noop = () => true;
 var GranuleRecipe = React.createClass({
   displayName: 'GranuleRecipe',
 
   propTypes: {
     params: React.PropTypes.object,
     dispatch: React.PropTypes.func,
-    granules: React.PropTypes.object
+    granules: React.PropTypes.object,
+    router: React.PropTypes.object
   },
 
   componentWillMount: function () {
@@ -23,6 +31,31 @@ var GranuleRecipe = React.createClass({
     if (!this.props.granules.map[granuleId]) {
       this.props.dispatch(getGranule(granuleId));
     }
+  },
+
+  navigateBack: function () {
+    const { router } = this.props;
+    router.push('/granules');
+  },
+
+  reprocess: function () {
+    const { granuleId } = this.props.params;
+    this.props.dispatch(reprocessGranule(granuleId));
+  },
+
+  reingest: function () {
+    const { granuleId } = this.props.params;
+    this.props.dispatch(reingestGranule(granuleId));
+  },
+
+  remove: function () {
+    const { granuleId } = this.props.params;
+    this.props.dispatch(removeGranule(granuleId));
+  },
+
+  delete: function () {
+    const { granuleId } = this.props.params;
+    this.props.dispatch(deleteGranule(granuleId));
   },
 
   render: function () {
@@ -36,32 +69,32 @@ var GranuleRecipe = React.createClass({
     const granule = record.data;
     const recipeOrder = get(granule, 'recipe.order', []);
 
-    // processing steps list items
-    const steps = recipeOrder.map((step) => <li key={step}>{step}</li>);
-
     // processing step blocks
-    const recipeSteps = recipeOrder.map((step, index) => (
-      <div key={step + index} className='recipe'>
-        <h3>Step {index + 1}: {step}</h3>
-        <p>Description: {granule.recipe[step].description}</p>
-        <Ace mode='json'
-          theme={config.editorTheme}
-          name='recipe-read-only'
-          readOnly={true}
-          value={JSON.stringify(granule.recipe[step], null, '\t')}
-          width='auto'
-          tabSize={config.tabSize}
-          showPrintMargin={false}
-          minLines={7}
-          maxLines={25}
-          wrapEnabled={true}
-        />
-      </div>
-    ));
+    const recipeSteps = recipeOrder.map((step, index) => {
+      const description = get(granule, ['recipe', step, 'description']);
+      return (
+        <div key={step + index} className='recipe'>
+          <label>Step {index + 1}: {step}</label>
+          {description ? <span className='label__description'>{description}</span> : null}
+          <Ace mode='json'
+            theme={config.editorTheme}
+            name='recipe-read-only'
+            readOnly={true}
+            value={JSON.stringify(granule.recipe[step], null, '\t')}
+            width='auto'
+            tabSize={config.tabSize}
+            showPrintMargin={false}
+            minLines={7}
+            maxLines={25}
+            wrapEnabled={true}
+          />
+        </div>
+      );
+    });
 
     const ingestedFiles = Object.keys(get(granule, 'files', {})).map((filename, index) => (
       <div key={filename + index}>
-        <h3>{filename}</h3>
+        <label>{filename}</label>
         <Ace mode='json'
           theme={config.editorTheme}
           name='recipe-read-only'
@@ -77,32 +110,44 @@ var GranuleRecipe = React.createClass({
       </div>
     ));
 
+    const dropdownConfig = [{
+      text: 'Reprocess',
+      action: this.reprocess,
+      status: get(this.props.granules.reprocessed, [granuleId, 'status']),
+      success: noop
+    }, {
+      text: 'Reingest',
+      action: this.reingest,
+      status: get(this.props.granules.reingested, [granuleId, 'status']),
+      success: noop
+    }, {
+      text: 'Remove from CMR',
+      action: this.remove,
+      status: get(this.props.granules.removed, [granuleId, 'status']),
+      success: noop
+    }, {
+      text: 'Delete',
+      action: this.delete,
+      disabled: granule.published,
+      status: get(this.props.granules.deleted, [granuleId, 'status']),
+      success: this.navigateBack
+    }];
+
     return (
       <div className='page__component'>
-
         <section className='page__section page__section__header-wrapper'>
           <h1 className='heading--large heading--shared-content with-description'>{granuleId}</h1>
-          <Link className='button button--small form-group__element--right button--disabled button--green' to='/'>Delete</Link>
-          <Link className='button button--small form-group__element--right button--green' to='/'>Remove from CMR</Link>
-          <Link className='button button--small form-group__element--right button--green' to='/'>Reprocess</Link>
+          <AsyncCommands config={dropdownConfig} />
           {lastUpdated(granule.queriedAt)}
         </section>
 
         <section className='page__section'>
-          <div className='heading__wrapper--border'>
-            <h2 className='heading--medium heading--shared-content with-description'>Recipe</h2>
-          </div>
-          <div>
-            <h3>Processing Order</h3>
-            <ol>{steps}</ol>
-            {recipeSteps}
-          </div>
+          <h2 className='heading--medium'>Recipe</h2>
+          {recipeSteps}
         </section>
 
         <section className='page__section'>
-          <div className='heading__wrapper--border'>
-            <h2 className='heading--medium heading--shared-content with-description'>Ingest</h2>
-          </div>
+          <h2 className='heading--medium'>Ingest</h2>
           <div>
             <h3>Duration: {seconds(granule.ingestDuration)}</h3>
             {ingestedFiles}
