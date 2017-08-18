e4919b060c0a8bffeb0f498bc0f4720afaf3660e
Derek Lieu <dl@dereklieu.com>
2017-08-18 15:41:54 -0400

    Allow re-ordering based on config

--- app/scripts/components/app/header.js
+++ app/scripts/components/app/header.js
@@ -1,12 +1,14 @@
 'use strict';
 import React from 'react';
+import c from 'classnames';
 import { Link } from 'react-router';
 import { get } from 'object-path';
 import { logout } from '../../actions';
-import { graphicsPath, exclude } from '../../config';
+import { graphicsPath, exclude, order } from '../../config';
 import { window } from '../../utils/browser';
 
 const excludeNav = get(exclude, 'nav', {});
+const navOrder = get(order, 'nav', {});
 
 const paths = [
   ['PDRs', '/pdrs'],
@@ -33,8 +35,13 @@ var Header = React.createClass({
     }
   },
 
-  active: function (path) {
-    return this.props.location.pathname.slice(0, path.length) === path ? 'active' : '';
+  className: function (path) {
+    const active = this.props.location.pathname.slice(0, path.length) === path;
+    const order = 'nav__order-' + (typeof navOrder[path] === 'undefined' ? 1 : navOrder[path]);
+    return c({
+      'active': active,
+      [order]: true
+    });
   },
 
   render: function () {
@@ -46,8 +53,10 @@ var Header = React.createClass({
           <h1 className='logo'><Link to='/'><img alt="Cumulus Logo" src={graphicsPath + 'layout/cumulus-logo.png'} /></Link></h1>
           <nav>
             { !this.props.minimal ? <ul>
-              {activePaths.map(path => <li key={path[0]} className={this.active(path[1])}><Link to={path[1]}>{path[0]}</Link></li>)}
-              <li className='rightalign'>{ authenticated ? <a onClick={this.logout}>Log out</a> : <Link to={'/login'}>Log in</Link> }</li>
+              {activePaths.map(path => <li
+                key={path[0]}
+                className={this.className(path[1])}><Link to={path[1]}>{path[0]}</Link></li>)}
+              <li className='rightalign nav__order-8'>{ authenticated ? <a onClick={this.logout}>Log out</a> : <Link to={'/login'}>Log in</Link> }</li>
             </ul> : <li>&nbsp;</li> }
           </nav>
         </div>
