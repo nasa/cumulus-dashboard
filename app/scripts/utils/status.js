5eaaee0a668606e9ce5829f03c883c92e6bfea01
Derek Lieu <dl@dereklieu.com>
2017-08-22 19:01:06 -0400

    Remove extra statuses

--- app/scripts/utils/status.js
+++ app/scripts/utils/status.js
@@ -1,24 +1,15 @@
 'use strict';
 const status = {
   '': '',
-  Ingesting: 'ingesting',
-  Processing: 'processing',
-  'Updating CMR': 'cmr',
-  Archiving: 'archiving',
+  Running: 'running',
   Completed: 'completed',
-  Stopped: 'stopped',
   Failed: 'failed'
 };
 export default status;
 
 export const queryStatus = [
-  'discovered',
-  'ingesting',
-  'processing',
-  'cmr',
-  'archiving',
+  'running',
   'completed',
-  'stopped',
   'failed'
 ];
 
