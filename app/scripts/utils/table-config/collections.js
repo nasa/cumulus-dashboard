bd0a2d33636347815938ebe834751b66df43abcb
Derek Lieu <dl@dereklieu.com>
2017-08-22 15:18:02 -0400

    Update collection table definitions

--- app/scripts/utils/table-config/collections.js
+++ app/scripts/utils/table-config/collections.js
@@ -7,32 +7,32 @@ import { deleteCollection } from '../../actions';
 
 export const tableHeader = [
   'Name',
+  'Version',
   'Granules',
   'Completed',
   'Failed',
-  'Average Duration',
-  'Created at',
-  'Updated at'
+  'Duration',
+  'Timestamp'
 ];
 
 export const tableRow = [
   (d) => <Link to={`/collections/collection/${d.name}/${d.version}`}>{d.name}</Link>,
-  (d) => tally(d.version),
-  (d) => tally(d.granules),
-  (d) => tally(get(d, 'granulesStatus.completed')),
-  (d) => tally(get(d, 'granulesStatus.failed')),
-  (d) => seconds(d.averageDuration),
-  (d) => fullDate(d.updatedAt)
+  'version',
+  (d) => tally(get(d, 'stats.properties.total')),
+  (d) => tally(get(d, 'stats.properties.completed')),
+  (d) => tally(get(d, 'stats.properties.failed')),
+  (d) => seconds(d.duration),
+  (d) => fullDate(d.timestamp)
 ];
 
 export const tableSortProps = [
   'name',
+  'version',
   null,
-  'granules',
   null,
   null,
-  'averageDuration',
-  'updatedAt'
+  'duration',
+  'timestamp'
 ];
 
 const confirmDelete = (d) => `Delete ${d} collections(s)?`;
