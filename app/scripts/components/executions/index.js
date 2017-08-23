d4732f12d2c7f7c0a417639ddacb4d6b81e17452
Derek Lieu <dl@dereklieu.com>
2017-08-23 13:33:35 -0400

    Create execution index and overview page

---
+++ app/scripts/components/executions/index.js
@@ -0,0 +1,36 @@
+'use strict';
+import React from 'react';
+import Sidebar from '../app/sidebar';
+
+var Executions = React.createClass({
+  propTypes: {
+    children: React.PropTypes.object,
+    location: React.PropTypes.object,
+    params: React.PropTypes.object
+  },
+
+  render: function () {
+    return (
+      <div className='page__workflows'>
+        <div className='content__header'>
+          <div className='row'>
+            <h1 className='heading--xlarge'>Executions</h1>
+          </div>
+        </div>
+        <div className='page__content'>
+          <div className='row wrapper__sidebar'>
+            <Sidebar
+              currentPath={this.props.location.pathname}
+              params={this.props.params}
+            />
+            <div className='page__content--shortened'>
+              {this.props.children}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+export default Executions;
