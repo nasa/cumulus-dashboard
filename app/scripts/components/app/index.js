8620db2bc9bce8df1cb68b04fb27aead0f90ce8f
Derek Lieu <dl@dereklieu.com>
2017-08-27 19:25:17 -0400

    Show the deploy target at dashboard top

--- app/scripts/components/app/index.js
+++ app/scripts/components/app/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { target } from '../../config';
+import { displayCase } from '../../utils/format';
 
 import Header from './header';
 
@@ -16,6 +18,11 @@ var App = React.createClass({
   render: function () {
     return (
       <div className='app'>
+        { target !== 'cumulus' ? (
+          <div className='app__target--container'>
+            <h4 className='app__target'>{displayCase(target)}</h4>
+          </div>
+        ) : null }
         <Header dispatch={this.props.dispatch} api={this.props.api} location={this.props.location}/>
         <main className='main' role='main'>
           {this.props.children}
