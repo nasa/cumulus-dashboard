a23f3afafacb250f14c1f064ee01056e19101ac4
Marc Huffnagle <marc@element84.com>
2017-09-15 17:37:19 -0400

    Fix lpdaac-uat graphics path

--- app/scripts/config/lpdaac-uat.js
+++ app/scripts/config/lpdaac-uat.js
@@ -2,7 +2,7 @@
 var logo = require('./logo');
 module.exports = {
   apiRoot: 'https://8ns3a35lha.execute-api.us-east-1.amazonaws.com/UAT/',
-  graphicsPath: '/dashboard/graphics/',
+  graphicsPath: '/graphics/',
   environment: 'lpdaac-uat',
   consoleMessage: logo
 };
