feda7c1d75f6814756a7b97ae7415ca730fd2c3c
Derek Lieu <dl@dereklieu.com>
2017-09-20 13:31:31 -0400

    Fix local dev api

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -1,7 +1,7 @@
 'use strict';
 var logo = require('./logo');
 module.exports = {
-  apiRoot: 'https://cumulus.developmentseed.org/api/',
+  apiRoot: 'https://cumulus.ds.io/api/',
   graphicsPath: '/dashboard/graphics/',
   environment: 'production',
   consoleMessage: logo
