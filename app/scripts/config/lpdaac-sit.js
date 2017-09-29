e4fda8e0a5bbf0a10c4d650759712bd890e0818e
Marc Huffnagle <marc@element84.com>
2017-09-29 18:23:25 -0400

    Adding lpdaac-sit config

---
+++ app/scripts/config/lpdaac-sit.js
@@ -0,0 +1,8 @@
+'use strict';
+var logo = require('./logo');
+module.exports = {
+  apiRoot: 'https://r7w07xpqw8.execute-api.us-east-1.amazonaws.com/SIT',
+  graphicsPath: '/graphics/',
+  environment: 'lpdaac-sit',
+  consoleMessage: logo
+};
