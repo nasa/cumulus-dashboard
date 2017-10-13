bcd3993126c5bf24cd991202288180b17529e7cd
Patrick Quinn <patrick@element84.com>
2017-10-13 16:45:38 -0400

    Use Earthdata Login

--- app/scripts/config/ghrc-uat.js
+++ app/scripts/config/ghrc-uat.js
@@ -4,5 +4,6 @@ module.exports = {
   apiRoot: 'https://b2fxt3cf7b.execute-api.us-east-1.amazonaws.com/UAT/',
   graphicsPath: '/graphics/',
   environment: 'ghrc-uat',
+  requireEarthdataLogin: true,
   consoleMessage: logo
 };
--- app/scripts/config/lpdaac-sit.js
+++ app/scripts/config/lpdaac-sit.js
@@ -4,5 +4,6 @@ module.exports = {
   apiRoot: 'https://r7w07xpqw8.execute-api.us-east-1.amazonaws.com/SIT/',
   graphicsPath: '/graphics/',
   environment: 'lpdaac-sit',
+  requireEarthdataLogin: true,
   consoleMessage: logo
 };
