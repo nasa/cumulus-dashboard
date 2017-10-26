a92fba307d7aa4ad1eb470e406d061e4c58cc6fb
sethvincent <sethvincent@gmail.com>
2017-10-26 12:05:37 -0400

    allow apiRoot from config.yml

--- app/scripts/config/index.js
+++ app/scripts/config/index.js
@@ -22,13 +22,22 @@ if (targets[target]) {
   config = Object.assign({}, config, targets[target]);
 }
 
-if (!config) config = {};
-config.apiRoot = apiRoot;
+if (apiRoot) {
+  config.apiRoot = apiRoot;
+}
 
 assert(typeof config.apiRoot, 'string', 'apiRoot string is required');
 
 if (target !== 'cumulus' && env !== 'development') {
   Object.assign(config, { graphicsPath: `/dashboard/${target}/graphics/` });
+}
+
+if (!config.graphicsPath) {
+  if (env === 'staging') {
+    config.graphicsPath = '/dashboard/dev/graphics/';
+  } else if (env === 'production') {
+    config.graphicsPath = '/dashboard/graphics/';
+  }
 }
 
 Object.assign(config, { target });
