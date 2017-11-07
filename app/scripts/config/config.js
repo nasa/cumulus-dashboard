6a85b522824043701c6bcad67fa026e292742f99
Scisco <scisco7@gmail.com>
2017-11-07 17:20:18 -0500

    add nav to config

--- app/scripts/config/config.js
+++ app/scripts/config/config.js
@@ -1,5 +1,9 @@
 module.exports = {
   target: process.env.DAAC_NAME || 'local',
   environment: process.env.STAGE || 'development',
+  nav: {
+    order: ['collections'],
+    exclude: ['pdrs']
+  },
   apiRoot: process.env.APIROOT || 'https://wjdkfyb6t6.execute-api.us-east-1.amazonaws.com/dev/'
 };
