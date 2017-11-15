52365fa349e3cee05b08c2e5ec8a0a5b32969d98
Scisco <scisco7@gmail.com>
2017-11-15 10:02:21 -0500

    fix execution status endpoint

--- app/scripts/actions/index.js
+++ app/scripts/actions/index.js
@@ -384,7 +384,7 @@ export const queryHistogram = (options) => wrapRequest(null, get, {
 export const listWorkflows = (options) => wrapRequest(null, get, 'workflows', WORKFLOWS);
 
 export const getExecutionStatus = (arn) => wrapRequest(null, get, {
-  url: url.resolve(root, 'executionstatus/' + arn)
+  url: url.resolve(root, 'executions/' + arn)
 }, EXECUTION_STATUS);
 
 export const listExecutions = (options) => wrapRequest(null, get, {
