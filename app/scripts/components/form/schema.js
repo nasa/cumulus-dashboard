ce242a5e74b6b4789bf6be3b9b7203f755ee9c07
Scisco <scisco7@gmail.com>
2017-11-29 13:01:02 -0500

    show fields that are not in the schema

--- app/scripts/components/form/schema.js
+++ app/scripts/components/form/schema.js
@@ -23,12 +23,24 @@ export const traverseSchema = function (schema, fn, path) {
 // create a copy of an object, where the copy adheres strictly to the schema.
 export const removeReadOnly = function (data, schema) {
   const readOnlyRemoved = {};
+  const schemaFields = [];
   traverseSchema(schema, function (property, meta, schemaProperty, path) {
+    schemaFields.push(property);
     if (!meta.readonly) {
       const accessor = path ? path + '.' + property : property;
       set(readOnlyRemoved, accessor, get(data, accessor));
     }
   });
+
+  // filter fields that are not in the schema
+  const esFields = ['queriedAt', 'timestamp', 'stats'];
+  const nonSchemaFields = Object.keys(data).filter(f => (
+    schemaFields.indexOf(f) === -1 && esFields.indexOf(f) === -1)
+  );
+
+  // add them to the list of fields
+  nonSchemaFields.forEach(f => set(readOnlyRemoved, f, get(data, f)));
+
   return readOnlyRemoved;
 };
 
