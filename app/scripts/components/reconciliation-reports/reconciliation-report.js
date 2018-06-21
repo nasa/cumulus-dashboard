8011697f5bc85b4f1d3830a5eba7ade6ce116fe2
Scisco <scisco7@gmail.com>
2018-06-21 15:07:40 -0400

    fix reconcillation individual page setup

--- app/scripts/components/reconciliation-reports/reconciliation-report.js
+++ app/scripts/components/reconciliation-reports/reconciliation-report.js
@@ -1,10 +1,14 @@
 'use strict';
+import url from 'url';
+import path from 'path';
 import React from 'react';
 import { connect } from 'react-redux';
 import {
   interval,
   getReconciliationReport
 } from '../../actions';
+import { nullValue } from '../../utils/format';
+import SortableTable from '../table/sortable';
 import Metadata from '../table/metadata';
 import Loading from '../app/loading-indicator';
 import ErrorReport from '../errors/report';
@@ -25,12 +29,36 @@ function joinArrayObjectProperty (property) {
 const metaAccessors = [
   ['Created', 'reportStartTime'],
   ['Status', 'status'],
-  ['OK file count', 'okFileCount'],
-  ['Only in S3', 'onlyInS3', joinArray],
-  ['Only in DynamoDB', 'onlyInDynamoDb', joinArrayObjectProperty('granuleId')]
+  ['OK file count', 'okFileCount']
 ];
 
-var ReconciliationReport = React.createClass({
+const tableHeaderS3 = [
+  'Filename',
+  'Bucket',
+  'S3 Link'
+];
+
+const tableRowS3 = [
+  (d) => d.filename,
+  (d) => d.bucket,
+  (d) => d ? <a href={d.path} target='_blank'>Link</a> : nullValue
+];
+
+const tableHeaderDynamoDb = [
+  'GranuleId',
+  'Filename',
+  'Bucket',
+  'S3 Link'
+];
+
+const tableRowDyanmoDb = [
+  (d) => d.granuleId,
+  (d) => d.filename,
+  (d) => d.bucket,
+  (d) => d ? <a href={d.path} target='_blank'>Link</a> : nullValue
+];
+
+const ReconciliationReport = React.createClass({
   propTypes: {
     reconciliationReports: React.PropTypes.object,
     dispatch: React.PropTypes.func,
@@ -65,6 +93,30 @@ var ReconciliationReport = React.createClass({
 
     const record = reconciliationReports.map[reconciliationReportName];
 
+    let filesInS3 = [];
+    let filesInDynamoDb = [];
+    if (record && record.data) {
+      console.log(record.data)
+      filesInS3 = record.data.onlyInS3.map(d => { 
+        const parsed = url.parse(d); 
+        return {
+          filename: path.basename(parsed.pathname),
+          bucket: parsed.hostname,
+          path: parsed.href
+        };
+      });
+
+      filesInDynamoDb = record.data.onlyInDyanmoDb.map(d => { 
+        const parsed = url.parse(d.uri); 
+        return {
+          granuleId: d.granuleId,
+          filename: path.basename(parsed.pathname),
+          bucket: parsed.hostname,
+          path: parsed.href
+        };
+      });
+    }
+
     let error;
     if (record && record.data) {
       error = record.data.error;
@@ -84,6 +136,34 @@ var ReconciliationReport = React.createClass({
             <h2 className='heading--medium with-description'>Reconciliation report</h2>
           </div>
           {!record || (record.inflight && !record.data) ? <Loading /> : <Metadata data={record.data} accessors={metaAccessors} />}
+        </section>
+
+        <section className='page__section'>
+          <div className='heading__wrapper--border'>
+            <h2 className='heading--medium heading--shared-content with-description'>
+              Files only in DynamoDB {!record || (record.inflight && !record.data) ? '' : `(${record.data.onlyInDynamoDb.length})`}
+            </h2>
+          </div>
+          <SortableTable
+            data={filesInDynamoDb}
+            header={tableHeaderDynamoDb}
+            row={tableRowDyanmoDb}
+            props={['granuleId', 'filename', 'bucket', 'link']}
+          />
+        </section>
+
+        <section className='page__section'>
+          <div className='heading__wrapper--border'>
+            <h2 className='heading--medium heading--shared-content with-description'>
+              Files only in S3 {!record || (record.inflight && !record.data) ? '' : `(${record.data.onlyInS3.length})`}
+            </h2>
+          </div>
+          <SortableTable
+            data={filesInS3}
+            header={tableHeaderS3}
+            row={tableRowS3}
+            props={['filename', 'bucket', 'link']}
+          />
         </section>
       </div>
     );
