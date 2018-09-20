5425937818a481f4efac5b9140c977a96aa4d703
Katherine Kelly <katherine.a.kelly@nasa.gov>
2018-09-20 12:34:39 -0400

    Added more testing of front end objects

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -3,5 +3,9 @@ describe('Dashboard Tests', () => {
   it('When not logged in it should redirect to login page', () => {
     cy.visit('http://localhost:3000');
     cy.url().should('include', '/#/auth');
+    cy.get('div[class=modal__internal]').within(() => {
+      cy.get('a').should('have.attr', 'href').and('include', 'token?');
+      cy.get('a').should('have.text', 'Login with Earthdata Login');
+    });
   });
 });
