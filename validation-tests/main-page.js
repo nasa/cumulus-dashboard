6f5bc0c75f239d296bab11add42693932334995f
Katherine Kelly <katherine.a.kelly@nasa.gov>
2018-09-26 15:56:39 -0400

    Renaming files and tests

--- validation-tests/main-page.js
+++ validation-tests/main-page.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 import http from 'ava-http';
 
-test('Hit the main page with no authenticated', async (t) => {
+test('POST, PUT, or DELETE operations with no session information are rejected', async (t) => {
   const res = await http.getResponse('http://localhost:3000/');
   t.is(res.statusCode, 200);
 
