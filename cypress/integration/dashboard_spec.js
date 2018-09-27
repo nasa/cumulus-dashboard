66c3b490288ce515d2596efee4902761b631accb
scisco <scisco7@gmail.com>
2018-09-27 12:51:15 -0400

    run all tests in docker

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,7 +1,8 @@
 
 describe('Dashboard Tests', () => {
+  const host = process.env.DASHBOARD_HOST || 'http://localhost:3000/';
   it('When not logged in it should redirect to login page', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit(host);
     cy.url().should('include', '/#/auth');
     cy.get('div[class=modal__internal]').within(() => {
       cy.get('a').should('have.attr', 'href').and('include', 'token?');
