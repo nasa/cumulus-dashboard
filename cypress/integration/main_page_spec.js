4318addfd4a6ce724e3fdf9222462023228399f4
Mark Boyd <markdavidboyd@gmail.com>
2018-10-18 14:45:49 -0400

    restore steps to main page cypress spec

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -21,6 +21,11 @@ describe('Dashboard Tests', () => {
   });
 
   it('Logging out successfully redirects to the login screen', () => {
+    cy.visit('/');
+    cy.get('div[class=modal__internal]').within(() => {
+      cy.get('a').click();
+    });
+
     cy.get('h1[class=heading--xlarge').should('have.text', 'CUMULUS Dashboard');
 
     cy.get('nav li').last().within(() => {
@@ -28,6 +33,8 @@ describe('Dashboard Tests', () => {
     });
     cy.get('nav li').last().click();
     cy.url().should('include', '/#/auth');
+
+    cy.visit('#/collections');
 
     cy.url().should('not.include', '/#/collections');
     cy.url().should('include', '/#/auth');
