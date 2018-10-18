e8fcff55172009c1736032c03691131200c2eb82
Mark Boyd <markdavidboyd@gmail.com>
2018-10-18 14:19:32 -0400

    update cypress Rules test to use login command

--- cypress/integration/rules.spec.js
+++ cypress/integration/rules.spec.js
@@ -1,7 +1,6 @@
 describe('Rules page', () => {
-  const host = process.env.DASHBOARD_HOST || 'http://localhost:3000/';
   it('when not logged in it should redirect to login page', () => {
-    cy.visit(`${host}#/rules`);
+    cy.visit('#/rules');
     cy.url().should('include', '/#/auth');
     cy.get('div[class=modal__internal]').within(() => {
       cy.get('a').should('have.attr', 'href').and('include', 'token?');
@@ -12,10 +11,7 @@ describe('Rules page', () => {
   describe('when logged in', () => {
     before(() => {
       cy.login();
-
-      cy.contains('Rules')
-        .click();
-      // cy.visit('#/rules');
+      cy.visit('#/rules');
     });
 
     it('should display a list of rules', () => {
