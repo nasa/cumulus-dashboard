6ca7968cb9744af2800221b34b23c4bc3aa7e94d
Mark Boyd <markdavidboyd@gmail.com>
2018-10-30 15:04:05 -0400

    update rules tests to use new command for editing jSON textareas

--- cypress/integration/rules.spec.js
+++ cypress/integration/rules.spec.js
@@ -30,7 +30,7 @@ describe('Rules page', () => {
       cy.get('nav').contains('Rules').click();
       cy.url().should('include', '/#/rules');
       cy.get('table tbody tr').should('have.length', 1);
-      cy.get(`table tr[data-value="${testRuleName}"]`)
+      cy.contains('table tr', testRuleName)
         .should('exist')
         .within(() => {
           cy.contains(testProviderId).should('exist');
@@ -43,8 +43,7 @@ describe('Rules page', () => {
 
     it('display a rule with the correct data', () => {
       cy.visit('/#/rules');
-      cy.get(`table tr[data-value="${testRuleName}"]`)
-        .contains(testRuleName)
+      cy.contains('table tr a', testRuleName)
         .click();
       cy.url().should('include', `/#/rules/rule/${testRuleName}`);
       cy.get('.metadata__details')
@@ -73,26 +72,42 @@ describe('Rules page', () => {
       cy.get('@addRule').should('have.attr', 'href', '#/rules/add');
       cy.get('@addRule').click();
 
-      cy.get('textarea').clear({force: true});
-      cy.get('textarea').clear({force: true});
-      const newRule = '{{}"name":"newRule","workflow":"HelloWorldWorkflow","provider":"PODAAC_SWOT","collection":{{}"name":"MOD09GQ","version":"006"},"meta":{{}},"rule":{{}"type":"onetime","value":""},"state":"ENABLED"}';
-      cy.get('textarea').type(newRule, {force: true});
+      const ruleName = 'newRule';
+      const workflow = 'HelloWorldWorkflow';
+      const provider = 'PODAAC_SWOT';
+      const collection = { name: 'MOD09GQ', version: '006' };
+      const newRule = {
+        name: ruleName,
+        workflow,
+        provider,
+        collection,
+        meta: {},
+        rule: {
+          type: 'onetime',
+          'value': ''
+        },
+        state: 'ENABLED'
+      };
+      cy.editJsonTextarea({ data: newRule });
       cy.get('form').get('input').contains('Submit').click();
 
       cy.contains('.heading--xlarge', 'Rules');
-      cy.contains('table tbody tr a', 'newRule')
+      cy.contains('table tbody tr a', ruleName)
         .should('exist')
-        .and('have.attr', 'href', '#/rules/rule/newRule').click();
+        .and('have.attr', 'href', `#/rules/rule/${ruleName}`).click();
 
       cy.contains('.heading--xlarge', 'Rules');
-      cy.contains('.heading--large', 'newRule');
+      cy.contains('.heading--large', ruleName);
       cy.contains('.heading--medium', 'Rule Overview');
-      cy.url().should('include', '#/rules/rule/newRule');
+      cy.url().should('include', `#/rules/rule/${ruleName}`);
       cy.get('.metadata__details')
         .within(() => {
-          cy.contains('RuleName').should('exist').next().should('have.text', 'newRule');
-          cy.contains('Workflow').should('exist').next().should('have.text', 'HelloWorldWorkflow');
-          cy.contains('Provider').should('exist').next().contains('a', 'PODAAC_SWOT').should('have.attr', 'href', '#/providers/provider/PODAAC_SWOT');
+          cy.contains('RuleName').should('exist').next().should('have.text', ruleName);
+          cy.contains('Workflow').should('exist').next().should('have.text', workflow);
+          cy.contains('Provider').should('exist')
+            .next()
+            .contains('a', provider)
+            .should('have.attr', 'href', `#/providers/provider/${provider}`);
         });
     });
 
@@ -110,7 +125,7 @@ describe('Rules page', () => {
         .get('button')
         .contains('Confirm')
         .click();
-      cy.get(`table tr[data-value="${testRuleName}"]`)
+      cy.contains('table tr a', testRuleName)
         .should('not.exist');
     });
   });
