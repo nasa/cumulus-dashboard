cb61e05d3255a8bfb07655098ba0284203f6435e
Mark Boyd <markdavidboyd@gmail.com>
2018-10-31 10:23:27 -0400

    remove unnecessary assertions from providers spec

--- cypress/integration/providers_spec.js
+++ cypress/integration/providers_spec.js
@@ -21,12 +21,12 @@ describe('Dashboard Providers Page', () => {
     it('displays a link to view providers', () => {
       cy.visit('/');
 
-      cy.contains('nav li a', 'Providers').should('exist').as('providers');
+      cy.contains('nav li a', 'Providers').as('providers');
       cy.get('@providers').should('have.attr', 'href', '#/providers');
       cy.get('@providers').click();
 
       cy.url().should('include', 'providers');
-      cy.contains('.heading--xlarge', 'Providers').should('exist');
+      cy.contains('.heading--xlarge', 'Providers');
 
       cy.get('table tbody tr').its('length').should('be.eq', 2);
     });
@@ -39,13 +39,13 @@ describe('Dashboard Providers Page', () => {
 
       cy.visit('/#/providers');
 
-      cy.contains('.heading--large', 'Provider Overview').should('exist');
-      cy.contains('a', 'Add a Provider').should('exist').as('addProvider');
+      cy.contains('.heading--large', 'Provider Overview');
+      cy.contains('a', 'Add a Provider').as('addProvider');
       cy.get('@addProvider').should('have.attr', 'href', '#/providers/add');
       cy.get('@addProvider').click();
 
-      cy.contains('.heading--xlarge', 'Providers').should('exist');
-      cy.contains('.heading--large', 'Create a provider').should('exist');
+      cy.contains('.heading--xlarge', 'Providers');
+      cy.contains('.heading--large', 'Create a provider');
 
       // fill the form and submit
       cy.get('form div ul').as('providerinput');
@@ -72,22 +72,19 @@ describe('Dashboard Providers Page', () => {
       cy.get('form div input[value=Submit]').click();
 
       // displays the new provider
-      cy.contains('.heading--xlarge', 'Providers').should('exist');
-      cy.contains('.heading--large', name).should('exist');
-      cy.contains('.heading--medium', 'Provider Overview').should('exist');
+      cy.contains('.heading--xlarge', 'Providers');
+      cy.contains('.heading--large', name);
+      cy.contains('.heading--medium', 'Provider Overview');
       cy.url().should('include', `#/providers/provider/${name}`);
       cy.get('.metadata__details')
         .within(() => {
           cy.contains('Global Connection Limit')
-            .should('exist')
             .next()
             .should('have.text', `${connectionLimit}`);
           cy.contains('Protocol')
-            .should('exist')
             .next()
             .should('have.text', protocol);
           cy.contains('Host')
-            .should('exist')
             .next()
             .contains('a', 'Link')
             .should('have.attr', 'href', host);
@@ -96,8 +93,7 @@ describe('Dashboard Providers Page', () => {
       // verify the new provider is added to the providers list
       cy.contains('a', 'Back to Provider').click();
       cy.contains('table tbody tr a', name)
-        .should('exist')
-        .and('have.attr', 'href', `#/providers/provider/${name}`);
+        .should('have.attr', 'href', `#/providers/provider/${name}`);
     });
 
     it('provider page has button to edit the provider', () => {
@@ -107,13 +103,13 @@ describe('Dashboard Providers Page', () => {
 
       cy.visit(`/#/providers/provider/${name}`);
       cy.contains('.heading--large', name);
-      cy.contains('a', 'Edit').should('exist').as('editprovider');
+      cy.contains('a', 'Edit').as('editprovider');
       cy.get('@editprovider')
         .should('have.attr', 'href')
         .and('include', `#/providers/edit/${name}`);
       cy.get('@editprovider').click();
 
-      cy.contains('.heading--large', `Edit ${name}`).should('exist');
+      cy.contains('.heading--large', `Edit ${name}`);
 
       cy.get('form div ul').as('providerinput');
       cy.get('@providerinput')
@@ -130,17 +126,15 @@ describe('Dashboard Providers Page', () => {
       cy.get('form div input[value=Submit]').click();
 
       // displays the updated provider
-      cy.contains('.heading--xlarge', 'Providers').should('exist');
-      cy.contains('.heading--large', name).should('exist');
-      cy.contains('.heading--medium', 'Provider Overview').should('exist');
+      cy.contains('.heading--xlarge', 'Providers');
+      cy.contains('.heading--large', name);
+      cy.contains('.heading--medium', 'Provider Overview');
       cy.get('.metadata__details')
         .within(() => {
           cy.contains('Global Connection Limit')
-            .should('exist')
             .next()
             .should('have.text', `${connectionLimit}`);
           cy.contains('Host')
-            .should('exist')
             .next()
             .contains('a', 'Link')
             .should('have.attr', 'href', host);
@@ -150,7 +144,7 @@ describe('Dashboard Providers Page', () => {
     it('provider page has button to delete the provider', () => {
       const name = 's3_provider';
       cy.visit(`/#/providers/provider/${name}`);
-      cy.contains('.heading--large', name).should('exist');
+      cy.contains('.heading--large', name);
 
       // delete provider
       cy.get('.dropdown__options__btn').click();
