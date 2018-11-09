229423500dee38f85b5e4c93c51687bb41b5b080
Matt Savoie <savoie@nsidc.org>
2018-11-09 11:28:40 -0700

    eslint shame :bell:

--- app/scripts/reducers/mmtLinks.js
+++ app/scripts/reducers/mmtLinks.js
@@ -12,10 +12,10 @@ export default function reducer (state = initialState, action) {
   state = Object.assign({}, state);
   const { data } = action;
   switch (action.type) {
-  case ADD_MMTLINK:
-    const collectionId = getCollectionId(data);
-    set(state, [collectionId], data.url);
-    break;
+    case ADD_MMTLINK:
+      const collectionId = getCollectionId(data);
+      set(state, [collectionId], data.url);
+      break;
   }
   return state;
 }
