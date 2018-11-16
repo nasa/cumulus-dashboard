94e926abffb97519cf266286060ff7ebf542126d
Jenny Liu <jenny.h.liu@nasa.gov>
2018-11-16 10:19:54 -0500

    compare data with fixture

--- cypress/integration/executions_spec.js
+++ cypress/integration/executions_spec.js
@@ -36,16 +36,36 @@ describe('Dashboard Executions Page', () => {
         .next().contains('li', '1 Running');
 
       // shows a list of executions with IDs and status
+      const executionStatusFile = './test/fake-api-fixtures/executions/index.json';
+      cy.readFile(executionStatusFile).as('executionStatus');
+
       cy.get('table tbody tr').as('list');
       cy.get('@list').its('length').should('be.eq', 5);
+
+      // compare data in each row with the data from fixture
       cy.get('@list').each(($el, index, $list) => {
-        cy.wrap($el).children().as('children');
-        cy.get('@children').its('length').should('be.eq', 6);
-        cy.get('@children').eq(0).children('a').should('have.attr', 'href');
-        cy.get('@children').eq(1).invoke('text').should('match', /(Completed|Failed|Running)/);
-        cy.get('@children').eq(2).invoke('text').should('match', /.+/);
-        cy.get('@children').eq(3).invoke('text').should('match', /.+ago$/);
-        cy.get('@children').eq(4).invoke('text').should('match', /.+s$/);
+        cy.wrap($el).children().as('rows');
+        cy.get('@rows').its('length').should('be.eq', 6);
+
+        cy.get('@executionStatus').its('results').then((executions) => {
+          const execution = executions[index];
+          cy.get('@rows').eq(0).children('a')
+            .should('have.attr', 'href')
+            .and('include', execution.arn);
+          cy.get('@rows').eq(0).children('a')
+            .should('have.attr', 'title')
+            .and('be.eq', execution.name);
+          cy.get('@rows').eq(1).invoke('text')
+            .should('be.eq', execution.status.replace(/^\w/, c => c.toUpperCase()));
+          cy.get('@rows').eq(2).invoke('text')
+            .should('be.eq', execution.type);
+          cy.get('@rows').eq(3).invoke('text')
+            .should('match', /.+ago$/);
+          cy.get('@rows').eq(4).invoke('text')
+            .should('be.eq', `${(Math.round(execution.duration * 100) / 100).toString()}s`);
+          cy.get('@rows').eq(5).invoke('text')
+            .should('be.eq', execution.collectionId);
+        });
       });
     });
   });
