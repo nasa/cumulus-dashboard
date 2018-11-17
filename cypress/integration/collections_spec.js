430b041e356318a5c20ef39a089a83a81ce5f039
Matt Savoie <savoie@nsidc.org>
2018-11-16 17:36:23 -0700

    Use cypress.route() to mock CMR responses.

--- cypress/integration/collections_spec.js
+++ cypress/integration/collections_spec.js
@@ -31,16 +31,25 @@ describe('Dashboard Collections Page', () => {
       cy.get('table tbody tr').its('length').should('be.eq', 5);
     });
 
-    it('should display correct MMT Links for collections list', () => {
+    it.only('should display correct MMT Links for collections list', () => {
+      cy.server();
+      cy.fixture('cmr').then((fixture) => {
+        fixture.forEach((call) => {
+          cy.route(call.method, call.url, call.body);
+        });
+      });
+
       cy.visit('/#/collections');
 
       cy.get('table tbody tr').its('length').should('be.eq', 5);
+
       cy.contains('table tbody tr', 'MOD09GQ').contains('td a', 'MMT')
         .should('have.attr', 'href')
-        .and('eq', 'https://mmt.uat.earthdata.nasa.gov/collections/C1218668453-CUMULUS');
+        .and('eq', 'https://mmt.uat.earthdata.nasa.gov/collections/CMOD09GQ-CUMULUS');
+
       cy.contains('table tbody tr', 'L2_HR_PIXC').contains('td a', 'MMT')
         .should('have.attr', 'href')
-        .and('eq', 'https://mmt.uat.earthdata.nasa.gov/collections/C1224299630-CUMULUS');
+        .and('eq', 'https://mmt.uat.earthdata.nasa.gov/collections/CL2_HR_PIXC-CUMULUS');
     });
 
     it('should add a new collection', () => {
