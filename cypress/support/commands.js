ef0a65ff61497b3bf4a7f880d317926f3dad5f0d
Mark Boyd <markdavidboyd@gmail.com>
2018-11-28 21:20:50 -0500

    update cy.login() command to set token using Redux action so that data store and localStorage are updated

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,6 +24,8 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+import { SET_TOKEN } from '../../app/scripts/actions';
+
 Cypress.Commands.add('login', () => {
   const authUrl = `${Cypress.config('baseUrl')}/#/auth`;
   cy.request({
@@ -35,9 +37,12 @@ Cypress.Commands.add('login', () => {
   }).then((response) => {
     const query = response.redirectedToUrl.substr(response.redirectedToUrl.indexOf('?') + 1);
     const token = query.split('=')[1];
-    cy.window()
-      .its('localStorage')
-      .invoke('setItem', 'auth-token', token);
+    cy.window().its('appStore').then((store) => {
+      store.dispatch({
+        type: SET_TOKEN,
+        token
+      });
+    });
   });
 });
 
