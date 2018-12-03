138c676a5d71c57355861b0e09b8f950cffafe01
Mark Boyd <markdavidboyd@gmail.com>
2018-12-03 14:38:13 -0500

    remove unnecessary line from fake API

--- fake-api.js
+++ fake-api.js
@@ -29,7 +29,6 @@ function fakeApiMiddleWare (req, res, next) {
 
   res.header('Access-Control-Allow-Origin', origin);
   res.header('Access-Control-Allow-Credentials', true);
-  // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin');
 
