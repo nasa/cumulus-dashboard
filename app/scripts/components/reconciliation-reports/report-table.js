87a36730f6698573c2f1711e676e524b5cc1cf14
Mark Boyd <markdavidboyd@gmail.com>
2019-03-18 23:01:07 -0400

    add show/hide functionality for report tables

--- app/scripts/components/reconciliation-reports/report-table.js
+++ app/scripts/components/reconciliation-reports/report-table.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Collapsible from 'react-collapsible';
 
 import SortableTable from '../table/sortable';
 
 class ReportTable extends React.Component {
   render () {
     const {
+      collapsible,
+      collapseThreshold,
       data,
       title,
       tableHeader,
@@ -17,17 +20,34 @@ class ReportTable extends React.Component {
       return null;
     }
 
+    let reportTable = (
+      <SortableTable
+        data={data}
+        header={tableHeader}
+        row={tableRow}
+        props={tableProps}
+      />
+    );
+
+    if (collapsible && data.length > collapseThreshold) {
+      reportTable = (
+        <Collapsible
+          trigger={`Show table (${data.length} rows)`}
+          triggerWhenOpen="Hide table"
+          triggerClassName={'button button--green button--small'}
+          triggerOpenedClassName={'button button--green button--small'}
+        >
+          { reportTable }
+        </Collapsible>
+      );
+    }
+
     return (
-      <div className='page__section--small'>
+      <div className='page__section--small report__table'>
         <h3 className='heading--small heading--shared-content with-description'>
           {title} ({data.length})
         </h3>
-        <SortableTable
-          data={data}
-          header={tableHeader}
-          row={tableRow}
-          props={tableProps}
-        />
+        { reportTable }
       </div>
     );
   }
@@ -39,6 +59,11 @@ ReportTable.propTypes = {
   tableHeader: PropTypes.array,
   tableRow: PropTypes.array,
   tableProps: PropTypes.array
+};
+
+ReportTable.defaultProps = {
+  collapsible: true,
+  collapseThreshold: 1
 };
 
 export default ReportTable;
