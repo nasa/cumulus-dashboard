25cebdcc04a03ea41b2e6192aba5eb4fa07e2212
Mark Boyd <markdavidboyd@gmail.com>
2019-04-12 12:49:34 -0400

    update docker build script to docker image for node v8 and ability to set APIROOT

--- bin/build_in_docker.sh
+++ bin/build_in_docker.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+
+# echo $APIROOT;
+# exit;
+
 set -evx
 
 DIST="$(pwd)/dist"
@@ -18,9 +22,6 @@ set -evx
 apt-get update
 
 apt-get install -y \
-  g++ \
-  make \
-  python \
   rsync
 
 mkdir /build
@@ -34,8 +35,9 @@ rsync -av \
   set -evx
 
   cd /build
-  npm install
-  ./node_modules/.bin/gulp
+  npm install -g yarn
+  yarn install
+  APIROOT=$APIROOT yarn run build
 
   rsync -av ./dist/ /dist/
   chown -R "${DOCKER_UID}:${DOCKER_GID}" /dist/
@@ -46,6 +48,8 @@ chmod +x tmp/script.sh
 echo "Building to $DIST"
 docker run \
   --rm \
+  -e APIROOT=$APIROOT
   --volume "${DIST}:/dist" \
   --volume "$(pwd):/cumulus-dashboard:ro" \
-  node:slim /cumulus-dashboard/tmp/script.sh
+  node:8-slim \
+  /cumulus-dashboard/tmp/script.sh
