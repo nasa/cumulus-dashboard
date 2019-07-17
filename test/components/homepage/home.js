e46f4f90a2d83b91ed29e7898f9766ea4a9bfcb0
Matt Savoie <savoie@nsidc.org>
2019-07-17 16:30:41 -0600

    Update component unit test.

--- test/components/homepage/home.js
+++ test/components/homepage/home.js
@@ -6,15 +6,18 @@ import React from 'react';
 import { shallow, configure } from 'enzyme';
 
 import { Home } from '../../../app/scripts/components/home';
+import { tally } from '../../../app/scripts/utils/format';
 
 configure({ adapter: new Adapter() });
 
 test('CUMULUS-799 Home page contains distribution error report', async (t) => {
   const dist = {
-    data: {
-      errors: 52,
-      successes: 43
-    }
+    apiGateway: {
+      execution: { errors: 10, successes: 123 },
+      access: { errors: 20, successes: 456 }
+    },
+    apiLambda: { errors: {}, successes: 789343 },
+    s3Access: { errors: {}, successes: 1011 }
   };
   const granules = { list: [] };
   const stats = {
@@ -25,19 +28,34 @@ test('CUMULUS-799 Home page contains distribution error report', async (t) => {
       data: {}
     }
   };
-  const dispatch = () => {};
+  const cumulusInstance = {stackName: 'stackName'};
+  const dispatch = () => Promise.resolve();
 
-  const home = shallow(<Home
-    dispatch={dispatch}
-    dist={dist}
-    granules={granules}
-    stats={stats}
-    />);
+  const home = shallow(
+    <Home
+      dispatch={dispatch}
+      dist={dist}
+      granules={granules}
+      stats={stats}
+      cumulusInstance={cumulusInstance}
+    />
+  );
+
   // TODO [MHS, 2019-07-16] This needs correcting, updating the fake api and the correct tag..
-  const metrics = home.find('#distMetrics li');
-  t.is(metrics.length, 2);
-  t.is(metrics.at(0).key(), 'Errors');
-  t.is(metrics.at(1).key(), 'Successes');
-  t.is(metrics.at(0).find('Link').dive().text(), `${dist.data.errors} Errors`);
-  t.is(metrics.at(1).find('Link').dive().text(), `${dist.data.successes} Successes`);
+  const errorMetrics = home.find('#distributionErrors li');
+  t.is(errorMetrics.length, 2);
+  t.is(errorMetrics.at(0).key(), 'Gateway Execution Errors');
+  t.is(errorMetrics.at(1).key(), 'Gateway Access Errors');
+  t.is(errorMetrics.at(0).find('Link').dive().text(), `${tally(dist.apiGateway.execution.errors)} Gateway Execution Errors`);
+  t.is(errorMetrics.at(1).find('Link').dive().text(), `${tally(dist.apiGateway.access.errors)} Gateway Access Errors`);
+  const successMetrics = home.find('#distributionSuccesses li');
+  t.is(successMetrics.length, 4);
+  t.is(successMetrics.at(0).key(), 'S3 Access Successes');
+  t.is(successMetrics.at(1).key(), 'Distribution API Lambda Successes');
+  t.is(successMetrics.at(2).key(), 'Gateway Execution Successes');
+  t.is(successMetrics.at(3).key(), 'Gateway Access Successes');
+  t.is(successMetrics.at(0).find('Link').dive().text(), `${tally(dist.s3Access.successes)} S3 Access Successes`);
+  t.is(successMetrics.at(1).find('Link').dive().text(), `${tally(dist.apiLambda.successes)} Distribution API Lambda Successes`);
+  t.is(successMetrics.at(2).find('Link').dive().text(), `${tally(dist.apiGateway.execution.successes)} Gateway Execution Successes`);
+  t.is(successMetrics.at(3).find('Link').dive().text(), `${tally(dist.apiGateway.access.successes)} Gateway Access Successes`);
 });
