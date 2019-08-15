2f282b0de117666c48c39ac5089e97114534de86
Matt Savoie <savoie@nsidc.org>
2019-08-15 08:10:04 -0600

    Test exteral links on homepage display.

--- test/components/homepage/home.js
+++ test/components/homepage/home.js
@@ -9,7 +9,6 @@ import { Home } from '../../../app/scripts/components/home';
 import { tally } from '../../../app/scripts/utils/format';
 import config from '../../../app/scripts/config';
 
-console.log(config);
 configure({ adapter: new Adapter() });
 
 test('CUMULUS-799 Home page contains distribution error report', async (t) => {
@@ -33,7 +32,7 @@ test('CUMULUS-799 Home page contains distribution error report', async (t) => {
   };
   const cumulusInstance = {stackName: 'stackName'};
   const dispatch = () => Promise.resolve();
-  config.kibanaRoot = '/fake/local/link/';
+  config.kibanaRoot = 'https://fake.com/linktokibana/';
   const home = shallow(
     <Home
       dispatch={dispatch}
@@ -48,8 +47,8 @@ test('CUMULUS-799 Home page contains distribution error report', async (t) => {
   t.is(errorMetrics.length, 3);
   t.is(errorMetrics.at(1).key(), 'Gateway Execution Errors');
   t.is(errorMetrics.at(2).key(), 'Gateway Access Errors');
-  t.is(errorMetrics.at(1).find('Link').dive().text(), `${tally(dist.apiGateway.execution.errors)} Gateway Execution Errors`);
-  t.is(errorMetrics.at(2).find('Link').dive().text(), `${tally(dist.apiGateway.access.errors)} Gateway Access Errors`);
+  t.is(errorMetrics.at(1).find('a').text(), `${tally(dist.apiGateway.execution.errors)} Gateway Execution Errors`);
+  t.is(errorMetrics.at(2).find('a').text(), `${tally(dist.apiGateway.access.errors)} Gateway Access Errors`);
   const successMetrics = home.find('#distributionSuccesses li');
   t.is(successMetrics.length, 5);
   t.is(successMetrics.at(0).key(), 'S3 Access Successes');
@@ -57,9 +56,9 @@ test('CUMULUS-799 Home page contains distribution error report', async (t) => {
   t.is(successMetrics.at(2).key(), 'Distribution API Lambda Successes');
   t.is(successMetrics.at(3).key(), 'Gateway Execution Successes');
   t.is(successMetrics.at(4).key(), 'Gateway Access Successes');
-  t.is(successMetrics.at(0).find('Link').dive().text(), `${tally(dist.s3Access.successes)} S3 Access Successes`);
-  t.is(successMetrics.at(1).find('Link').dive().text(), `${tally(dist.teaLambda.successes)} TEA Lambda Successes`);
-  t.is(successMetrics.at(2).find('Link').dive().text(), `${tally(dist.apiLambda.successes)} Distribution API Lambda Successes`);
-  t.is(successMetrics.at(3).find('Link').dive().text(), `${tally(dist.apiGateway.execution.successes)} Gateway Execution Successes`);
-  t.is(successMetrics.at(4).find('Link').dive().text(), `${tally(dist.apiGateway.access.successes)} Gateway Access Successes`);
+  t.is(successMetrics.at(0).find('a').text(), `${tally(dist.s3Access.successes)} S3 Access Successes`);
+  t.is(successMetrics.at(1).find('a').text(), `${tally(dist.teaLambda.successes)} TEA Lambda Successes`);
+  t.is(successMetrics.at(2).find('a').text(), `${tally(dist.apiLambda.successes)} Distribution API Lambda Successes`);
+  t.is(successMetrics.at(3).find('a').text(), `${tally(dist.apiGateway.execution.successes)} Gateway Execution Successes`);
+  t.is(successMetrics.at(4).find('a').text(), `${tally(dist.apiGateway.access.successes)} Gateway Access Successes`);
 });
