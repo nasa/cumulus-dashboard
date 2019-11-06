59b68a2f23c34dae5943ce554925ac168791b73b
Katherine Kelly <katherine.a.kelly@nasa.gov>
2019-11-06 15:22:10 -0500

    logout icon fix

--- app/src/js/components/Header/header.js
+++ app/src/js/components/Header/header.js
@@ -66,7 +66,7 @@ class Header extends React.Component {
               {activePaths.map(path => <li
                 key={path[0]}
                 className={this.className(path[1])}><Link to={path[1]}>{path[0]}</Link></li>)}
-              <li className='rightalign nav__order-8'>{ authenticated ? <a onClick={this.logout}><FontAwesomeIcon icon="sign-out-alt" />Log out</a> : <Link to={'/login'}>Log in</Link> }</li>
+              <li className='rightalign nav__order-8'>{ authenticated ? <a onClick={this.logout}><span className="log-icon"></span>Log out</a> : <Link to={'/login'}>Log in</Link> }</li>
             </ul> : <li>&nbsp;</li> }
           </nav>
         </div>
