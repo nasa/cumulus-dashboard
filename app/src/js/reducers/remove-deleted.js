57d17291e9616c8aacf4c50c3df12130fdd6dc17
Mark Boyd <markdavidboyd@gmail.com>
2020-01-07 17:04:37 -0500

    fix export style for file to file

--- app/src/js/reducers/remove-deleted.js
+++ app/src/js/reducers/remove-deleted.js
@@ -2,7 +2,7 @@
 import { get } from 'object-path';
 // manually filter out list items that have been deleted.
 // https://github.com/nasa/cumulus-dashboard/issues/276
-module.exports = function removeDeleted (accessor, list, deleted) {
+export const removeDeleted = (accessor, list, deleted) => {
   const filterByDeletedSuccess = function (item) {
     const id = get(item, accessor);
     let record = deleted[id];
