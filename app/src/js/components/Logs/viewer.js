92d5fc1be677201e8f050cb02561c0fe51709522
Chuck Daniels <chuckwondo@users.noreply.github.com>
2020-01-10 15:23:32 -0500

    Fix log search on granule detail page (#622)
    The UI no longer breaks (producing a blank page) when the user types in the
    log search box on the granule details page.
    
    CUMULUS-1679

--- app/src/js/components/Logs/viewer.js
+++ app/src/js/components/Logs/viewer.js
@@ -1,4 +1,6 @@
 'use strict';
+
+import truncate from 'lodash.truncate';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { interval, getLogs, clearLogs } from '../../actions';
@@ -16,140 +18,160 @@ const noLogs = {
 };
 // const twoDays = 2 * 1000 * 60 * 60 * 24;
 
-const statusOptions = [
-  'All',
-  'Debug',
-  'Info',
-  'Warn',
-  'Error',
-  'Fatal',
-  'Trace'
-];
+// Use a Map to preserve order of entries.  Otherwise, when using a plain
+// object, during iteration over the keys (or entries), "numeric" keys are
+// yielded in numeric order, not in insertion order (Gah!).
+const logLevels = new Map([
+  ['[10 TO 60]', 'All'],
+  ['60', 'Fatal'],
+  ['50', 'Error'],
+  ['40', 'Warn'],
+  ['30', 'Info'],
+  ['20', 'Debug'],
+  ['10', 'Trace'],
+]);
+
+/**
+ * Returns the name of the specified "numeric" log level, or the specified
+ * level converted to a string if no such level is defined.
+ *
+ * @param {number | string} level "numeric" log level to get the name of
+ * @returns {string} name of the specified log level or the level converted to
+ *    a string if no such level is defined
+ */
+const logLevelName = (level) => (logLevels.get(`${level}`) || `${level}`);
 
 class LogViewer extends React.Component {
-  constructor () {
-    super();
+  constructor(props) {
+    super(props);
+
     this.displayName = 'LogViewer';
-    this.setSearch = this.setSearch.bind(this);
-    this.setSearchLevel = this.setSearchLevel.bind(this);
+
     this.query = this.query.bind(this);
+    this.cancelInterval = () => { };
+
     this.state = {
-      level: 'All',
-      search: ''
+      level: logLevels.keys().next().value,
+      search: '',
     };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.query();
   }
 
-  componentDidUpdate (prevProps) {
-    if (JSON.stringify(this.props.query) !== JSON.stringify(prevProps.query)) {
-      const query = this.props.query || {};
-      this.query(query);
-    }
-
-    if (this.props.logs.error && this.cancelInterval) {
+  componentDidUpdate() {
+    if (this.props.logs.error) {
       this.cancelInterval();
-      this.cancelInterval = null;
+      this.cancelInterval = () => { };
     }
   }
 
-  componentWillUnmount () {
-    if (this.cancelInterval) { this.cancelInterval(); }
+  componentWillUnmount() {
+    this.cancelInterval();
     this.props.dispatch(clearLogs());
   }
 
-  setSearch (e) {
-    const value = e.currentTarget.value;
-    this.setState({ search: value }, this.query);
+  setSearch(search) {
+    this.setState({ search }, this.query);
   }
 
-  setSearchLevel (id, value) {
-    this.setState({ search: '', level: value }, this.query);
+  setSearchLevel(level) {
+    this.setState({ level }, this.query);
   }
 
-  query () {
-    const query = this.props.query || {};
-    const { search, level } = this.state;
-    if (search) {
-      query.q = this.state.search;
-    } else if (level && level !== 'All') {
-      query.level = level.toLowerCase();
-    }
+  query() {
     const { dispatch } = this.props;
-    if (this.cancelInterval) { this.cancelInterval(); }
+    const { search, level } = this.state;
+    const query = Object.assign({}, this.props.query);
+
+    if (search || query.q) {
+      // Since the API ignores most other parameters when the `q` parameter is
+      // specified, we must add the search query and log level directly to the
+      // `q` parameter to ensure they are included in the query.  In addition,
+      // enclosing `q` value in double-quotes to avoid potential undesired
+      // interaction with the remainder of the constructed search query.
+      query.q = [
+        query.q && `"${query.q}"`,
+        search && `(${search})`,
+        `level:${level}`,
+      ].filter(Boolean).join(' AND ');
+    } else {
+      query.level = level;
+    }
+
+    this.cancelInterval();
     dispatch(clearLogs());
 
     // let isFirstPull = true;
-    function querySinceLast () {
+    function querySinceLast() {
       // on first pull, get the last 48 hours
 
       // deactivating until timestamp filter works again on the API side
       // const duration = isFirstPull ? twoDays : logsUpdateInterval;
       // const from = moment().subtract(duration, 'milliseconds').format();
       // isFirstPull = false;
-      return dispatch(getLogs(Object.assign({ }, query)));
+      dispatch(getLogs(query));
     }
+
     this.cancelInterval = interval(querySinceLast, logsUpdateInterval, true);
   }
 
-  render () {
+  render() {
     const { logs, notFound } = this.props;
-    let items = logs.items;
-    if (!items.length && !logs.inflight) {
-      let placeholder = notFound ? Object.assign({}, noLogs, {
-        displayText: notFound
-      }) : noLogs;
-      items = [placeholder];
-    }
-    const count = logs.items.length ? tally(items.length) : 0;
     const { level } = this.state;
+    const count = tally(logs.items.length);
+    const items = (logs.items.length || logs.inflight)
+      ? logs.items
+      : [Object.assign({}, noLogs, notFound ? { displayText: notFound } : {})];
+
     return (
-      <section className='page__section'>
+      <section className='page__section' >
         <div className='heading__wrapper--border'>
-          <h2 className='heading--medium heading--shared-content with-description'>Logs
-            <span className='num--title'>{logs.inflight ? <LoadingEllipsis /> : `${count}`}</span>
+          <h2 className='heading--medium heading--shared-content with-description'>
+            Logs <span className='num--title'>{logs.inflight ? <LoadingEllipsis /> : count}</span>
           </h2>
+
           <form className='search__wrapper form-group__element form-group__element--right form-group__element--right--sm form-group__element--small'>
             <input
               className='search'
               type='search'
               placeholder='Search all logs'
               value={this.state.search}
-              onChange={this.setSearch}/>
-            <span className='search__icon'></span>
+              onChange={(e) => this.setSearch(e.target.value)} />
+            <span className='search__icon' />
           </form>
 
           <form className='search__wrapper form-group__element form-group__element--right form-group__element--right--sm form-group__element--small'>
             <Dropdown
               label={'Level'}
               value={level}
-              options={statusOptions}
+              options={Array.from(logLevels.entries())}
               id={'logs-viewer-dropdown'}
-              onChange={this.setSearchLevel}
+              onChange={(_, level) => this.setSearchLevel(level)}
               noNull={true}
             />
           </form>
         </div>
-        {logs.error ? <ErrorReport report={logs.error} truncate={true} /> : null}
-        <div className='logs'>
-          {items.map((d, i) => {
-            let text = d.displayText;
-            if (text.length > 200) {
-              text = text.slice(0, 200) + '...';
-            }
-            let level;
-            if (d.level) {
-              if (d.level === 30) level = 'info';
-              else level = d.level.toString().toLowerCase();
-            } else {
-              level = 'info';
-            }
 
-            return <p
-              key={d.key}
-              className='logs__item'><span className='logs__item--date'>{d.displayTime}</span> <span className={'logs__item--level logs__item--' + level}>{level}</span> {text}</p>;
+        {logs.error && <ErrorReport report={logs.error} truncate={true} />}
+
+        <div className='logs'>
+          {items.map((item) => {
+            const text = truncate(item.displayText, { length: 200 });
+            const level = logLevelName(item.level);
+
+            return (
+              <p key={item.key} className='logs__item'>
+                <span className='logs__item--date'>
+                  {item.displayTime}
+                </span>
+                <span className={`logs__item--level logs__item--${level.toLowerCase()}`}>
+                  {` ${level.toUpperCase()} `}
+                </span>
+                {text}
+              </p>
+            );
           })}
         </div>
       </section>
