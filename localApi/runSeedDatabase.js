cebdbfb7c95d537a0b89c8c7d810478ca8a6f0cd
Matt Savoie <savoie@nsidc.org>
2020-01-14 15:17:31 -0700

    Update for new reconcilation-reports.
    Updated cumulus-core to load two sample reconcilation-reports that are served
    by the localApi.

--- localApi/runSeedDatabase.js
+++ localApi/runSeedDatabase.js
@@ -2,6 +2,7 @@
 
 const { seedEverything } = require('../cypress/plugins/seedEverything');
 
+
 seedEverything()
   .then(() => console.log('Seeded Database with cypress fixtures'))
-  .catch((e) => console.error(`unable to seed database ${e}`));
+  .catch((e) => console.error(`Unable to seed database ${e}`));
