d83e5a7d1c603c496655c21412860cfda1796dce
Matt Savoie <savoie@nsidc.org>
2020-03-16 15:04:23 -0600

    Set default time range to last 24 hours.

--- app/src/js/utils/datepicker.js
+++ app/src/js/utils/datepicker.js
@@ -1,6 +1,21 @@
 'use strict';
 
 export const urlDateFormat = 'YYYYMMDDHHmmSS';
+export const secondsPerDay = 60.0 * 60.0 * 24.0;
+
+export const allDateRanges = [
+  {value: 'All', label: 'All'},
+  {value: 'Custom', label: 'Custom'},
+  {value: 1 / 24.0, label: 'Last hour'},
+  {value: 1, label: 'Last 24 hours'},
+  {value: 7, label: 'Last week'},
+  {value: 30, label: 'Last 30 Days'},
+  {value: 60, label: 'Last 60 days'},
+  {value: 180, label: 'Last 180 days'},
+  {value: 366, label: 'Last year'}
+];
+export const allHourFormats = ['12HR', '24HR'];
+export const dateTimeFormat = 'YYYY-MM-DDTHH:mm:ss.sss';
 
 // These are created as lists of objects so that we can keep them together when
 // mapping over the possible values.
