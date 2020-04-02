a8c5097854191947272cfa224caae3cf5d740449
Juanisa McCoy <juanisa.j.mccoy@nasa.gov>
2020-04-02 15:16:10 -0400

    styling for rules overview page

--- app/src/js/components/Rules/index.js
+++ app/src/js/components/Rules/index.js
@@ -17,7 +17,7 @@ class Rules extends React.Component {
         <div className='content__header'>
           <div className='row'>
             <h1 className='heading--xlarge heading--shared-content'>Rules</h1>
-            {showSidebar ? <Link className='button button--large button--white button__addcollections button__arrow button__animation' to='/rules/add'>Add a rule</Link> : null}
+            {showSidebar}
           </div>
         </div>
         <div className='page__content'>
