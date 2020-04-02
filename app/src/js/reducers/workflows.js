81f73478eea01d33574da36fe45f5af254feba04
Matt Savoie <savoie@nsidc.org>
2020-04-02 16:54:11 -0600

    Update Changelog.

--- app/src/js/reducers/workflows.js
+++ app/src/js/reducers/workflows.js
@@ -25,6 +25,13 @@ function createMap (data) {
   return map;
 }
 
+/**
+ * Filters intput data by filterstring on the data'a object's name.
+*
+ * @param {Array} rawData - An array of objects with a name parameter.
+ * @param {string} filterString - a string to check if name includes.
+ * @returns {Array} Filtered Array of rawData's objects who's names include filterString.
+ */
 export const filterData = (rawData, filterString) => {
   if (filterString !== null) {
     return rawData.filter(d => d.name.includes(filterString));
