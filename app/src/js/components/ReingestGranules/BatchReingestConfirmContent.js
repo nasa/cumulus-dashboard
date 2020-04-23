d86e0e135e90d86ae5159cf59cfae07e7e0228c1
Matt Savoie <savoie@nsidc.org>
2020-04-23 17:57:47 -0600

    Code Cleanup.

--- app/src/js/components/ReingestGranules/BatchReingestConfirmContent.js
+++ app/src/js/components/ReingestGranules/BatchReingestConfirmContent.js
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const maxDisplayed = 10;
-const pluralRequest = 'You have submitted a request to reingest the following granules.';
-const singularRequest = 'You have submitted a request to reingest the following granule.';
-
-const pluralConfirm = 'Are you sure that you want to reingest these granules?\nNote: These granule files will be overwritten.';
-const singularConfirm = 'Are you sure that you want to reingest this granule?\nNote: The granule file will be overwritten.';
 
 const BatchReingestConfirmContent = ({ selected = [] }) => {
-  const isMultiple = (selected.length > 1);
-  const requestText = isMultiple ? pluralRequest : singularRequest;
-  const confirmText = isMultiple ? pluralConfirm : singularConfirm;
+  const isMultiple = selected.length > 1;
+  const these = isMultiple ? 'These' : 'This';
+  const s = isMultiple ? 's' : '';
+  const requestText = `You have submitted a request to reingest the following granule${s}.`;
+  const confirmText = `Are you sure that you want to reingest ${these.toLowerCase()} granule${s}?\nNote: ${these} granule file${s} will be overwritten.`;
 
   return (
     <>
@@ -20,12 +17,16 @@ const BatchReingestConfirmContent = ({ selected = [] }) => {
         {selected.map((selection, index) => {
           if (index < maxDisplayed) {
             return <li key={index}>{selection}</li>;
-          } return <></>;
+          }
+          return <></>;
         })}
-        {(selected.length > maxDisplayed) &&
-         <li key={maxDisplayed}>and {selected.length - maxDisplayed} more.</li>}
+        {selected.length > maxDisplayed && (
+          <li key={maxDisplayed}>and {selected.length - maxDisplayed} more.</li>
+        )}
       </ul>
-      {confirmText.split('\n').map((line, index) => <p key={index}>{line}</p>)}
+      {confirmText.split('\n').map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
     </>
   );
 };
