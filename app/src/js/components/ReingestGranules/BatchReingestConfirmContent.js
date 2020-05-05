1a986c9287292f87ba7124497e27e121b39f8691
Danielle Peters <dopeter1@mbp-dopeter1.edn.ecs.nasa.gov>
2020-05-05 10:08:28 -0400

    Reingest granules modal styling

--- app/src/js/components/ReingestGranules/BatchReingestConfirmContent.js
+++ app/src/js/components/ReingestGranules/BatchReingestConfirmContent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Alert } from 'react-bootstrap';
 
 export const maxDisplayed = 10;
 
@@ -8,7 +9,7 @@ const BatchReingestConfirmContent = ({ selected = [] }) => {
   const these = isMultiple ? 'These' : 'This';
   const s = isMultiple ? 's' : '';
   const requestText = `You have submitted a request to reingest the following granule${s}.`;
-  const confirmText = `Are you sure that you want to reingest ${these.toLowerCase()} granule${s}?\nNote: ${these} granule file${s} will be overwritten.`;
+  const confirmText = `Are you sure that you want to reingest ${these.toLowerCase()} granule${s}?`;
 
   const displayedItems = () => {
     const items = [];
@@ -23,6 +24,7 @@ const BatchReingestConfirmContent = ({ selected = [] }) => {
 
   return (
     <>
+      <Alert variant='primary'><strong>Attention:</strong> {`The selected granule${s} will be overwritten`}</Alert>
       {requestText}
       <ul>
         {displayedItems()}
