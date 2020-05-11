282131a385276549c4b381b8960caa9751558014
Matt Savoie <savoie@nsidc.org>
2020-05-11 11:53:04 -0600

    Prevents accidental overwrite of this.cancelInterval

--- app/src/js/components/home.js
+++ app/src/js/components/home.js
@@ -55,7 +55,7 @@ class Home extends React.Component {
 
   componentDidMount () {
     const { dispatch } = this.props;
-    this.cancelInterval = interval(this.query, updateInterval, true);
+    this.refreshQuery();
     dispatch(getCumulusInstanceMetadata())
       .then(() => {
         dispatch(getDistApiGatewayMetrics(this.props.cumulusInstance));
