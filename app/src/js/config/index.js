64e7360e5bcf8e55aa0f9b5015563e4fc3ed3fbe
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2020-05-26 16:06:49 -0400

    Dashboard release 1.9.0

--- app/src/js/config/index.js
+++ app/src/js/config/index.js
@@ -10,7 +10,7 @@ const baseConfig = {
   environment: 'development',
   requireEarthdataLogin: false,
   apiRoot: 'https://wjdkfyb6t6.execute-api.us-east-1.amazonaws.com/dev/',
-  minCompatibleApiVersion: '1.22.1',
+  minCompatibleApiVersion: '1.23.2',
   oauthMethod: 'earthdata',
 
   graphicsPath: '/src/assets/images/',
