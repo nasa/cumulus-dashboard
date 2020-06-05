e4ad59a79705026d02050f829801fb0455aebd15
Mark Boyd <markdavidboyd@gmail.com>
2020-06-05 14:22:55 -0400

    add placeholder for bumping minimum required API version

--- app/src/js/config/index.js
+++ app/src/js/config/index.js
@@ -10,7 +10,7 @@ const baseConfig = {
   environment: 'development',
   requireEarthdataLogin: false,
   apiRoot: 'https://wjdkfyb6t6.execute-api.us-east-1.amazonaws.com/dev/',
-  minCompatibleApiVersion: '1.23.2',
+  minCompatibleApiVersion: 'change-me-next-api-release',
   oauthMethod: 'earthdata',
 
   graphicsPath: '/src/assets/images/',
