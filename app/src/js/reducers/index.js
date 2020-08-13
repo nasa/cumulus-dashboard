45c5d8b6dc6afb6117321074acbb48c7631512c2
Danielle Peters <danielle.o.peters@nasa.gov>
2020-08-13 17:10:32 -0400

    CUMULUS-1884 Make sidebar collapsible (#837)

--- app/src/js/reducers/index.js
+++ app/src/js/reducers/index.js
@@ -24,6 +24,7 @@ import rules from './rules';
 import reconciliationReports from './reconciliation-reports';
 import mmtLinks from './mmtLinks';
 import cumulusInstance from './cumulus-instance';
+import sidebar from './sidebar';
 
 const def = (state, _action) => state || {};
 
@@ -41,6 +42,7 @@ export const reducers = {
   granuleCSV,
   stats,
   timer,
+  sidebar,
   pdrs,
   providers,
   logs,
