0fa4126f463d1c9c1427fa71bbb7e9892e37c7cc
Matt Savoie <savoie@nsidc.org>
2020-10-19 15:33:18 -0600

    Fix for broken test on should select a different collection.
    What I found  was that the order of the collections changed, and it changed out
    from under the click that was to happen right after the should('have.attr')
    statement.
    
    So the test was asserting that the href existed correctly, but it was updated
    out from underneath the test before the click could happen

--- cypress/integration/collections_spec.js
+++ cypress/integration/collections_spec.js
@@ -171,17 +171,12 @@ describe('Dashboard Collections Page', () => {
       // collections with which to populate the dropdown on the collection
       // details page.
       cy.visit('/collections');
-      // TODO [DOP, 2020-05-14] Workaround until CUMULUS-1996 is resolved
-      // Stop timer so that it does not dispatch listCollections and change
-      // the order of items in the list after date is cleared
-      cy.get('.form__element__updateToggle .form__element__clickable').click();
-      cy.clearStartDateTime();
       cy.wait('@getCollections');
       cy.get('.table .tbody .tr').should('have.length', 5);
 
       cy.contains('.table .tbody .tr a', name)
-        .should('have.attr', 'href', `/collections/collection/${name}/${version}`)
-        .click();
+        .should('have.attr', 'href', `/collections/collection/${name}/${version}`);
+      cy.get(`[data-value=${name}___${version}] > .table__main-asset > a`).click();
       cy.contains('.heading--large', `${name} / ${version}`);
       cy.contains('.heading--large', 'Granule Metric');
 
