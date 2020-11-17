6c1655acd5073e80b3c3b28db7a74c8c3709f94b
Jenny Liu <jenny.h.liu@nasa.gov>
2020-11-17 15:44:58 -0500

    list action

--- app/src/js/components/ListActions/ListActions.js
+++ app/src/js/components/ListActions/ListActions.js
@@ -32,52 +32,6 @@ const ListActions = ({
     }
   }
 
-  // TODO collapsible actions
-  /*
-  return (
-    <div className={`list-action-wrapper${!hasActions || !children ? ' no-actions' : ''}`}>
-      {children}
-      <div className='list-actions'>
-        {hasActions && (
-          <div className='form--controls'>
-            {bulkActions.map((item, index) => {
-              const { Component, text } = item;
-              return (
-                <React.Fragment key={text || index}>
-                  {Component && React.cloneElement(Component, { selected })}
-                  {!Component &&
-                    <BatchAsyncCommand
-                      dispatch={dispatch}
-                      action={item.action}
-                      state={item.state}
-                      text={item.text}
-                      clearError={item.clearError}
-                      confirm={item.confirm}
-                      confirmOptions={item.confirmOptions}
-                      getModalOptions={item.getModalOptions}
-                      onSuccess={handleBulkActionSuccess}
-                      onError={handleBulkActionError}
-                      selected={selected}
-                      className={item.className || ''}
-                    />
-                  }
-                </React.Fragment>
-              );
-            })}
-          </div>
-        )}
-        <Timer
-          noheader={!hasActions}
-          dispatch={dispatch}
-          action={action}
-          config={queryConfig}
-          reload={completedBulkActions}
-        />
-      </div>
-    </div>
-  );
-  */
-
   function renderBulkActions() {
     return bulkActions.map((item, index) => {
       const { Component, text } = item;
