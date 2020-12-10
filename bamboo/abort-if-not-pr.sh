2057a0dd8ad87230e795fc0786db7cbecdd4adcf
Matt Savoie <savoie@nsidc.org>
2020-12-10 09:35:34 -0700

    Remove abort-if-not-pr.sh script
    Instead of checking in each task, just abort failure at the very beginning.
    This prevents problems with artifact creation.

--- bamboo/abort-if-not-pr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-source .bamboo_env_vars || true
-if [[ $GIT_PR != true ]]; then
-  >&2 echo "******Branch HEAD is not a github PR targeting $PR_BRANCH skipping step"
-  exit 0
-fi
