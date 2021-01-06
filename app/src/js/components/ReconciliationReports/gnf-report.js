23c675ba2c67b6ac694d721c7fb2e806bef65605
Jenny Liu <jenny.h.liu@nasa.gov>
2021-01-06 13:28:37 -0500

    inventory report

--- app/src/js/components/ReconciliationReports/gnf-report.js
+++ app/src/js/components/ReconciliationReports/gnf-report.js
@@ -105,7 +105,7 @@ const GnfReport = ({
             Granules Not Found <span className="num-title">{totalMissingGranules}</span>
           </h2>
         </div>
-        <div className="filters">
+        <div className="list-action-wrapper filters">
           <Search
             action={searchReconciliationReport}
             clear={clearReconciliationSearch}
