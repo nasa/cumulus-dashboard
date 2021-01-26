081ad20670dad56295e54b37d7ca9ac56a3cd125
Brian Tennity <brian+git@element84.com>
2021-01-25 19:58:17 -0500

    [CUMULUS-2148] Add retry limits and timeouts to original solution

--- app/src/js/utils/table-config/reconciliation-reports.js
+++ app/src/js/utils/table-config/reconciliation-reports.js
@@ -11,7 +11,7 @@ export const tableColumns = ({ dispatch, isGranules, query }) => ([
     Header: 'Name',
     accessor: 'name',
     Cell: ({ cell: { value }, row: { original: { type } } }) => { // eslint-disable-line react/prop-types
-      const link = (location) => ({ pathname: `/reconciliation-reports/report/${encodeURIComponent(value)}`, search: getPersistentQueryParams(location) });
+      const link = (location) => ({ pathname: `/reconciliation-reports/report/${value}`, search: getPersistentQueryParams(location) });
       switch (type) {
         case 'Internal':
           return <Link to={link} onClick={(e) => handleDownloadClick(e, value, dispatch)}>{value}</Link>;
@@ -64,6 +64,8 @@ export const tableColumns = ({ dispatch, isGranules, query }) => ([
   }
 ]);
 
+const MAX_REPORT_RETRIES = 5;
+
 const handleDownloadClick = (e, reportName, dispatch) => {
   e.preventDefault();
   dispatch(getReconciliationReport(reportName)).then((response) => {
@@ -85,12 +87,30 @@ const handleCsvDownloadClick = (e, reportName, dispatch) => {
 const handleDeleteClick = (e, value, dispatch, query) => {
   e.preventDefault();
   dispatch(deleteReconciliationReport(value)).then((response) => {
-    setTimeout(
-      () => dispatch(listReconciliationReports(query)),
-      1000
-    );
+    if (!response.error) {
+      refreshReports(dispatch, value, query, MAX_REPORT_RETRIES);
+    }
   });
 };
+
+// It can take an extra second or two to delete a report from
+// Elasticsearch. This function keeps checking the reports to make sure it was
+// deleted before displaying the delete result.
+const refreshReports = (dispatch, value, query, retries) => {
+  if (retries > 0) {
+    dispatch(listReconciliationReports(query)).then((reports) => {
+      const reportDeleted = checkReportDeleted(value, reports.data.results);
+      if (!reportDeleted) {
+        setTimeout(
+          () => refreshReports(dispatch, value, query, retries - 1),
+          1000
+        );
+      }
+    });
+  }
+};
+
+const checkReportDeleted = (reportName, reports) => reports.every((report) => report.name !== reportName);
 
 export const bulkActions = (reports) => [];
 
