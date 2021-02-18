43c98d8ee3d1ddb22f4969698d2db5334cffea44
Danielle Peters <dopeter1@mbp-dopeter1.edn.ecs.nasa.gov>
2021-02-18 16:56:15 -0500

    Add remaining cypress tests

--- cypress/integration/logs_spec.js
+++ cypress/integration/logs_spec.js
@@ -2,6 +2,8 @@ const collectionName = 'MOD09GQ';
 const collectionVersion = '006';
 const granuleId = 'MOD09GQ.A0501579.PZB_CG.006.8580266395214';
 const executionArn = 'arn:aws:states:us-east-1:123456789012:execution:TestSourceIntegrationIngestAndPublishGranuleStateMachine-yCAhWOss5Xgo:b313e777-d28a-435b-a0dd-f1fad08116t1';
+const pdr = 'MOD09GQ_1granule_v3.PDR_test-test-src-integration-IngestGranuleDuplicateHandling-1582837549352';
+const provider = 's3_provider';
 
 const logSuccess = () => cy.intercept('GET', '/logs', {
   fixture: 'logs-success.json',
@@ -87,6 +89,30 @@ describe('Dashboard Logs', () => {
       cy.contains('.sidebar li', 'Logs').should('not.exist');
       cy.contains('.meta__row dt', 'Logs').should('not.exist');
       cy.contains('.meta__row dd a', 'View Execution Logs').should('not.exist');
+    });
+
+    it('should display logs on a PDR page when metrics are configured', () => {
+      logSuccess();
+      cy.visit(`/pdrs/pdr/${pdr}`);
+      cy.get('.page__section--logs').should('exist');
+    });
+
+    it('should not display logs on a PDR page when metrics are not configured', () => {
+      logError();
+      cy.visit(`/pdrs/pdr/${pdr}`);
+      cy.get('.page__section--logs').should('not.exist');
+    });
+
+    it('should display logs on a Provider page when metrics are configured', () => {
+      logSuccess();
+      cy.visit(`/providers/provider/${provider}`);
+      cy.get('.page__section--logs').should('exist');
+    });
+
+    it('should not display logs on a Provider page when metrics are not configured', () => {
+      logError();
+      cy.visit(`/providers/provider/${provider}`);
+      cy.get('.page__section--logs').should('not.exist');
     });
   });
 });
