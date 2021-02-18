ec7308eceb424688eb1d113a5692ee0780d9ab1d
Danielle Peters <dopeter1@mbp-dopeter1.edn.ecs.nasa.gov>
2021-02-18 14:49:55 -0500

    Add logs_spec and fixtures

---
+++ cypress/integration/logs_spec.js
@@ -0,0 +1,92 @@
+const collectionName = 'MOD09GQ';
+const collectionVersion = '006';
+const granuleId = 'MOD09GQ.A0501579.PZB_CG.006.8580266395214';
+const executionArn = 'arn:aws:states:us-east-1:123456789012:execution:TestSourceIntegrationIngestAndPublishGranuleStateMachine-yCAhWOss5Xgo:b313e777-d28a-435b-a0dd-f1fad08116t1';
+
+const logSuccess = () => cy.intercept('GET', '/logs', {
+  fixture: 'logs-success.json',
+  statusCode: 200
+});
+
+const logError = () => cy.intercept('GET', '/logs', {
+  fixture: 'logs-error.json',
+  statusCode: 400
+});
+
+describe('Dashboard Logs', () => {
+  describe('When logged in', () => {
+    before(() => {
+      cy.visit('/');
+      cy.task('resetState');
+    });
+
+    beforeEach(() => {
+      cy.login();
+      cy.intercept('GET', '/logs').as('getLogs');
+    });
+
+    it('should display logs on a Collection page when metrics are configured', () => {
+      logSuccess();
+      cy.visit(`/collections/collection/${collectionName}/${collectionVersion}`);
+      cy.contains('.sidebar li a', 'Logs').should('exist').click();
+      cy.url().should('include', '/logs');
+      cy.get('.page__section--logs').should('exist');
+    });
+
+    it('should not display logs on a Collection page when metrics are not configured', () => {
+      logError();
+      cy.visit(`/collections/collection/${collectionName}/${collectionVersion}`);
+      cy.contains('.sidebar li', 'Logs').should('not.exist');
+    });
+
+    it('should display logs on a Granule page when metrics are configured', () => {
+      logSuccess();
+      cy.visit(`/granules/granule/${granuleId}`);
+      cy.get('.page__section--logs').should('exist');
+    });
+
+    it('should not display logs on a Granule page when metrics are not configured', () => {
+      logError();
+      cy.visit(`/granules/granule/${granuleId}`);
+      cy.get('.page__section--logs').should('not.exist');
+    });
+
+    it('should display logs on a Granule status pages when metrics are configured', () => {
+      logSuccess();
+      cy.visit('/granules/completed');
+      cy.get('.page__section--logs').should('exist');
+      cy.visit('/granules/processing');
+      cy.get('.page__section--logs').should('exist');
+      cy.visit('/granules/failed');
+      cy.get('.page__section--logs').should('exist');
+    });
+
+    it('should not display logs on a Granule status pages when metrics are not configured', () => {
+      logError();
+      cy.visit('/granules/completed');
+      cy.get('.page__section--logs').should('not.exist');
+      cy.visit('/granules/processing');
+      cy.get('.page__section--logs').should('not.exist');
+      cy.visit('/granules/failed');
+      cy.get('.page__section--logs').should('not.exist');
+    });
+
+    it('should display logs on an Execution page when metrics are configured', () => {
+      logSuccess();
+      cy.visit(`/executions/execution/${executionArn}`);
+      cy.contains('.sidebar li', 'Logs').should('exist');
+      cy.contains('.meta__row dt', 'Logs').should('exist');
+      cy.contains('.meta__row dd a', 'View Execution Logs').should('exist').click();
+      cy.url().should('include', '/logs');
+      cy.contains('.status--process h2', 'Execution Logs').should('exist');
+    });
+
+    it('should not display logs on an Execution page when metrics are not configured', () => {
+      logError();
+      cy.visit(`/executions/execution/${executionArn}`);
+      cy.contains('.sidebar li', 'Logs').should('not.exist');
+      cy.contains('.meta__row dt', 'Logs').should('not.exist');
+      cy.contains('.meta__row dd a', 'View Execution Logs').should('not.exist');
+    });
+  });
+});
