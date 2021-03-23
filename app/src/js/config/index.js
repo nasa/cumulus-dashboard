90efa16685c8bb2812cdc85eb9111c79f4fc82fb
Matt Savoie <savoie@nsidc.org>
2021-03-23 15:09:05 -0600

    Version bumps for release v5.0.0

--- app/src/js/config/index.js
+++ app/src/js/config/index.js
@@ -8,7 +8,7 @@ const deploymentConfig = require('./config');
 const baseConfig = {
   environment: 'development',
   requireEarthdataLogin: false,
-  minCompatibleApiVersion: '5.0.0',
+  minCompatibleApiVersion: '7.0.0',
   oauthMethod: 'earthdata',
 
   graphicsPath: '/src/assets/images/',
