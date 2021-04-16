f26b8dabcb4f196a0d1b4430ed806569bc50fddc
Jenny Liu <jenny.h.liu@nasa.gov>
2021-04-15 20:09:49 -0400

    CUMULUS-2348 compatible version

--- app/src/js/config/index.js
+++ app/src/js/config/index.js
@@ -8,7 +8,7 @@ const deploymentConfig = require('./config');
 const baseConfig = {
   environment: 'development',
   requireEarthdataLogin: false,
-  minCompatibleApiVersion: '7.0.0',
+  minCompatibleApiVersion: 'change-me-next-api-release-after-8.0.0',
   oauthMethod: 'earthdata',
 
   graphicsPath: '/src/assets/images/',
