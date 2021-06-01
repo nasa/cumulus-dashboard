3f54789e5e3693050eb88b6ae001b532d34c8215
Matt Savoie <savoie@colorado.edu>
2021-06-01 16:29:08 -0600

    Yanks out most of the kibana generated links.
    3 kibana Link generation functions remain, but they all return the same basic
    link to kibana root's discovery page.

--- ava-conditional.config.cjs
+++ ava-conditional.config.cjs
@@ -6,10 +6,6 @@ module.exports = {
   environmentVariables: {
     HIDE_PDR: 'false',
     KIBANAROOT: 'https://fake.com/linktokibana',
-    KIBANA_CLOUDWATCH_UUID: 'fakeCloudwatchUUid',
-    KIBANA_DISTRIBUTION_UUID: 'fakeDistributionUUID',
-    KIBANA_GRANULES_UUID: 'fakeGranulesUUID',
-    KIBANA_SECURITY_TENANT: 'FakeSecuritySenant',
     AUTH_METHOD: 'launchpad',
     ESROOT: 'https://example.com',
     ES_CLOUDWATCH_INDEX_PATTERN: 'fakeESCloudwatchPattern*',
