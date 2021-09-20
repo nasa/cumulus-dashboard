6cc0bd0bd38f9946d160f14910b2d6db87a9aca0
Matt Savoie <savoie@colorado.edu>
2021-09-20 11:35:28 -0600

    Change find to filter to prevent undefined selectedGranules.

--- app/src/js/components/Granules/overview.js
+++ app/src/js/components/Granules/overview.js
@@ -102,7 +102,7 @@ class GranulesOverview extends React.Component {
     };
     const { granules, config } = this.props;
     const { selected } = this.state;
-    const selectedGranules = selected.map((id) => granules.list.data.find((g) => id === g.granuleId));
+    const selectedGranules = selected.map((id) => granules.list.data.filter((g) => id === g.granuleId));
     let actions = bulkActions(granules, actionConfig, selectedGranules);
     if (config.enableRecovery) {
       actions = actions.concat(recoverAction(granules, actionConfig));
