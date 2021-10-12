4edabdafd3fef82422aba6c138e1f6eb294efc1b
Danielle Peters <danielle@element84.com>
2021-10-12 10:45:40 -0400

    Update workflow test

--- cypress/integration/workflows_spec.js
+++ cypress/integration/workflows_spec.js
@@ -58,21 +58,21 @@ describe('Dashboard Workflows Page', () => {
       /* eslint-enable no-template-curly-in-string */
     });
 
-    it('filters workflows when a user types in the search box', () => {
+    it.only('filters workflows when a user types in the search box', () => {
       cy.intercept('GET', '/workflows*').as('get-workflows');
       cy.visit('/workflows');
       cy.get('.table .tbody .tr').should('have.length', 2);
       cy.get('.table .tbody .tr').first().contains('HelloWorldWorkflow');
       cy.get('.search').click().type('condtes');
-      cy.get('.table .tbody .tr').first().contains('SecondTestWorkflow');
       cy.get('.table .tbody .tr').should('have.length', 1);
+      cy.get('.table .tbody .tr').first().contains('SecondTestWorkflow');
     });
 
     it('filters workflows when a user comes to the page with a search query', () => {
       cy.intercept('GET', '/workflows*').as('get-workflows');
       cy.visit('/workflows?search=condtes');
-      cy.get('.table .tbody .tr').first().contains('SecondTestWorkflow');
       cy.get('.table .tbody .tr').should('have.length', 1);
+      cy.get('.table .tbody .tr').first().contains('SecondTestWorkflow');
     });
   });
 });
