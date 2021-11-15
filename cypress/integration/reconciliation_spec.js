1a879ccc60eb261c216035698f49cc87a7fe84e4
Danielle Peters <danielle@element84.com>
2021-11-15 10:49:15 -0500

    Fix cypress tests

--- cypress/integration/reconciliation_spec.js
+++ cypress/integration/reconciliation_spec.js
@@ -303,17 +303,17 @@ describe('Dashboard Reconciliation Reports Page', () => {
       cy.contains('.heading--medium', 'Total Conflict Comparisons');
       cy.contains('.num-title', '11');
 
-      cy.get('.table .th').eq(1).should('contain', 'Collection ID');
-      cy.get('.table .th').eq(2).should('contain', 'Granule ID');
-      cy.get('.table .th').eq(3).should('contain', 'S3');
-      cy.get('.table .th').eq(4).should('contain', 'Cumulus');
-      cy.get('.table .th').eq(5).should('contain', 'CMR');
+      cy.get('.table .th').eq(0).should('contain', 'Collection ID');
+      cy.get('.table .th').eq(1).should('contain', 'Granule ID');
+      cy.get('.table .th').eq(2).should('contain', 'S3');
+      cy.get('.table .th').eq(3).should('contain', 'Cumulus');
+      cy.get('.table .th').eq(4).should('contain', 'CMR');
 
-      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(1).should('contain', 'MOD09GQ___006');
-      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(2).should('contain', 'MOD09GQ.A0002421.oD4zvB.006.4281362831355');
+      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(0).should('contain', 'MOD09GQ___006');
+      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(1).should('contain', 'MOD09GQ.A0002421.oD4zvB.006.4281362831355');
+      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(2).find('span').should('have.class', 'status-indicator--failed');
       cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(3).find('span').should('have.class', 'status-indicator--failed');
-      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(4).find('span').should('have.class', 'status-indicator--failed');
-      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(5).find('span').should('have.class', 'status-indicator--success');
+      cy.get('.table .tbody .tr[data-value="MOD09GQ.A0002421.oD4zvB.006.4281362831355"] .td').eq(4).find('span').should('have.class', 'status-indicator--success');
 
       /** Legend */
       cy.get('.legend').should('have.length', 1);
