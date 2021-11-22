9e6249a47e404d35f517c3a70aaa839e6d9aadaa
Danielle Peters <danielle@element84.com>
2021-11-22 09:47:33 -0500

    Revert ava env config changes

--- ava-conditional.config.cjs
+++ ava-conditional.config.cjs
@@ -4,13 +4,13 @@ module.exports = {
   ...baseConfig,
   files: ['test/**/condition*', '!node_modules/', '!test/fixtures/**/*'],
   environmentVariables: {
-    HIDE_PDR: false,
+    HIDE_PDR: 'false',
     KIBANAROOT: 'https://fake.com/linktokibana',
     AUTH_METHOD: 'launchpad',
     ESROOT: 'https://example.com',
     ES_CLOUDWATCH_TARGET_PATTERN: 'fakeESCloudwatchPattern*',
     ES_DISTRIBUTION_TARGET_PATTERN: 'fakeESDistributionPattern*',
-    SHOW_DISTRIBUTION_API_METRICS: true,
+    SHOW_DISTRIBUTION_API_METRICS: 'true',
     BUCKET: 'https://example.com/bucket',
     STAGE: 'production',
     DAAC_NAME: 'Cumulus_Daac'
