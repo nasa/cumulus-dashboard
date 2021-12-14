7748a793f07ed08e31c2e7f7be648f901fe6947e
Danielle Peters <danielle.o.peters@nasa.gov>
2021-12-14 10:02:58 -0500

    [CUMULUS-2747] Show relevant create provider fields (#1029)

--- app/src/js/components/Providers/add.js
+++ app/src/js/components/Providers/add.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
@@ -9,23 +9,55 @@ import { isValidProvider } from '../../utils/validate';
 
 const SCHEMA_KEY = 'provider';
 
-const AddProvider = ({ providers }) => (
-  <div className="add_providers">
-    <Helmet>
-      <title> Add Provider </title>
-    </Helmet>
-    <AddRecord
-      baseRoute="/providers/provider"
-      createRecord={createProvider}
-      primaryProperty="id"
-      schemaKey={SCHEMA_KEY}
-      state={providers}
-      title="Create a provider"
-      validate={isValidProvider}
-      validationError={'Concurrent Connection Limit cannot be a negative value'}
-    />
-  </div>
-);
+const nonSftpExcludes = ['privateKey', 'cmKeyId'];
+const nonHttpsExcludes = ['allowedRedirects', 'certificateUri'];
+const s3Excludes = [
+  ...nonSftpExcludes,
+  ...nonHttpsExcludes,
+  'username',
+  'password',
+  'port',
+];
+
+const AddProvider = ({ providers }) => {
+  const [exclude, setExclude] = useState(nonSftpExcludes);
+  const [selectedProtocol, setSelectedProtocol] = useState('http');
+
+  const handleInputChange = {
+    protocol: (value) => {
+      if (value === 's3') setExclude(s3Excludes);
+      else if (value.includes('http')) setExclude(nonSftpExcludes);
+      else if (value === 'sftp') setExclude(nonHttpsExcludes);
+      else if (value === 'ftp') setExclude([...nonHttpsExcludes, ...nonSftpExcludes]);
+      setSelectedProtocol(value);
+    },
+  };
+
+  const data = {
+    protocol: selectedProtocol,
+  };
+
+  return (
+    <div className="add_providers">
+      <Helmet>
+        <title> Add Provider </title>
+      </Helmet>
+      <AddRecord
+        baseRoute="/providers/provider"
+        createRecord={createProvider}
+        data={data}
+        exclude={exclude}
+        handleInputChange={handleInputChange}
+        primaryProperty="id"
+        schemaKey={SCHEMA_KEY}
+        state={providers}
+        title="Create a provider"
+        validate={isValidProvider}
+        validationError="Concurrent Connection Limit cannot be a negative value"
+      />
+    </div>
+  );
+};
 
 AddProvider.propTypes = {
   providers: PropTypes.object,
