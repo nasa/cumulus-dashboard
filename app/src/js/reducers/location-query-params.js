86cd4afed311e1a54fee7783b0b29287cdb1f9af
Anthony Ortega <jose.a.ortega@nasa.gov>
2021-12-17 09:46:41 -0600

    CUMULUS-2543: Added unit test for LOCATION_CHANGE reducer and cleaned up reducer

--- app/src/js/reducers/location-query-params.js
+++ app/src/js/reducers/location-query-params.js
@@ -3,21 +3,16 @@ import { LOCATION_CHANGE } from 'connected-react-router';
 
 export const initialState = {
   search: {},
-  prevLocationPathname: '',
 };
 
 export default createReducer(initialState, {
   [LOCATION_CHANGE]: (state, action) => {
-    const { prevLocationPathname } = state;
     const newLocationPathname = action.payload.location?.pathname;
     const newLocationQuery = action.payload.location?.search;
 
-    if (newLocationPathname && prevLocationPathname === newLocationPathname) {
+    if (newLocationPathname) {
       // Capture latest filter for current view
       state.search[newLocationPathname] = newLocationQuery;
-    } else {
-      // Navigating to a new view
-      state.prevLocationPathname = newLocationPathname;
     }
   },
 });
