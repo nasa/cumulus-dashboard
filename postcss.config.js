eeefae863678c4c05f4bc9b13d263f4306776cb3
Filip Graniczny <filip@filipgraniczny.com>
2022-07-27 14:41:25 -0700

    remove precss from postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
 const autoprefixer = require('autoprefixer');
-const precss = require('precss');
 const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
-    precss,
     autoprefixer({
       overrideBrowserslist: ['> 0.2%', 'last 2 versions']
     }),
