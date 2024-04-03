7c4a70bb8d5209eef18ac73b691801902c934b29
jennyhliu <jenny.h.liu@nasa.gov>
2024-04-02 20:39:12 -0400

    update minCompatibleApiVersion

--- app/src/js/config/index.js
+++ app/src/js/config/index.js
@@ -8,7 +8,7 @@ const deploymentConfig = require('./config');
 const baseConfig = {
   environment: 'development',
   requireEarthdataLogin: false,
-  minCompatibleApiVersion: 'change-me',
+  minCompatibleApiVersion: '18.2.0',
   oauthMethod: 'earthdata',
 
   graphicsPath: '/src/assets/images/',
