43760ba462c3005adfa7b7372c9589162da2f51b
gacondino <gaerrielle105@gmail.com>
2024-06-20 09:38:45 -0400

    refactoring Delete Modal

--- app/src/js/components/DeleteCollection/DeleteCollectionModal.js
+++ app/src/js/components/DeleteCollection/DeleteCollectionModal.js
@@ -1,65 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Alert } from 'react-bootstrap';
+import { Alert, Modal } from 'react-bootstrap';
 import DefaultModal from '../Modal/modal';
 
-class DeleteCollectionModal extends React.Component {
-  constructor (props) {
-    super(props);
-
-    this.handleCancel = this.handleCancel.bind(this);
-    this.handleConfirm = this.handleConfirm.bind(this);
+const DeleteCollectionModal = ({
+  onCancel,
+  onConfirm,
+  show,
+  collectionLabel = ''
+}) => {
+  function handleCancel() {
+    onCancel();
+  }
+  function handleConfirm() {
+    onConfirm();
   }
 
-  handleCancel () {
-    this.props.onCancel();
-  }
-
-  handleConfirm () {
-    this.props.onConfirm();
-  }
-
-  render () {
-    return (
-      <DefaultModal
-        showModal = {this.props.show}
-        onCloseModal={this.handleCancel}
-        className={'delete-collection-modal'}
-        hasCancelButton={false}
-        hasConfirmButton={false}
-        title='Delete Collection'
-        onHide={this.handleCancel}
-        aria-labelledby="modal__delete-collection-modal"
-        onCancel={this.handleCancel}
-        onConfirm={this.handleConfirm}
-      >
-        <Alert variant="warning"><strong>Warning:</strong> This action can not be reversed once you submit it.</Alert>
-        <p> You have submitted a request to delete the following collection: </p>
-        <br />
-        <h1>
-          <strong>{this.props.collectionLabel}</strong>
-        </h1>
-        <br />
-        <p> Are you sure you want to permanently delete this collection? </p>
-        <button
+  return (
+    <DefaultModal
+      showModal = {show}
+      onCloseModal={handleCancel}
+      className={'delete-collection-modal'}
+      hasCancelButton={false}
+      hasConfirmButton={false}
+      title='Delete Collection'
+      onHide={handleCancel}
+      aria-labelledby="modal__delete-collection-modal"
+      onCancel={handleCancel}
+      onConfirm={handleConfirm}
+    >
+      <Modal.Body>
+      <Alert variant="warning"><strong>Warning:</strong> This action can not be reversed once you submit it.</Alert>
+      <p> You have submitted a request to delete the following collection: </p>
+      <Modal.Header className="d-flex justify-content-center">
+        <strong>{collectionLabel}</strong>
+      </Modal.Header>
+      <br />
+      <p> Are you sure you want to permanently delete this collection? </p>
+      </Modal.Body>
+      <Modal.Footer className="d-flex justify-content-center">
+      <button
           className='button button--cancel button__animation--md button__arrow button__arrow--md button__animation button--secondary form-group__element--left'
-          label="cancel"
-          onClick={this.handleCancel}
-        >
-            Cancel Request
-        </button>
-        <button
-          className='button button__deletecollections button__animation--md button__arrow button__arrow--md button__animation button__arrow--white'
-          label="confirm"
-          onClick={this.handleConfirm}
-        >
-            Delete Collection
-        </button>
-      </DefaultModal>
-
-    );
-  }
-}
+          onClick={handleCancel}
+      >
+          Cancel Request
+      </button>
+      <button
+        className='button button__deletecollections button__animation--md button__arrow button__arrow--md button__animation button__arrow--white'
+        onClick={handleConfirm}
+      >
+          Delete Collection
+      </button>
+      </Modal.Footer>
+    </DefaultModal>
+  );
+};
 
 DeleteCollectionModal.propTypes = {
   collectionLabel: PropTypes.string,
