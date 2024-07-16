047f146be53e2c8da9769120c43b0b73c76e373c
gacondino <gaerrielle105@gmail.com>
2024-07-16 15:11:29 -0400

    refactored delete collection modal

--- app/src/js/components/DeleteCollection/DeleteCollectionModal.js
+++ app/src/js/components/DeleteCollection/DeleteCollectionModal.js
@@ -3,63 +3,44 @@ import PropTypes from 'prop-types';
 import { Alert } from 'react-bootstrap';
 import DefaultModal from '../Modal/modal';
 
-class DeleteCollectionModal extends React.Component {
-  constructor (props) {
-    super(props);
-
-    this.handleCancel = this.handleCancel.bind(this);
-    this.handleConfirm = this.handleConfirm.bind(this);
-  }
-
-  handleCancel () {
-    this.props.onCancel();
-  }
-
-  handleConfirm () {
-    this.props.onConfirm();
-  }
-
-  render () {
-    return (
+const DeleteCollectionModal = ({ collectionLabel, onCancel, onConfirm, show }) => (
       <DefaultModal
-        showModal = {this.props.show}
-        onCloseModal={this.handleCancel}
+        showModal = {show}
+        onCloseModal={onCancel}
         className={'delete-collection-modal'}
         hasCancelButton={false}
         hasConfirmButton={false}
         title='Delete Collection'
-        onHide={this.handleCancel}
+        onHide={onCancel}
         aria-labelledby="modal__delete-collection-modal"
-        onCancel={this.handleCancel}
-        onConfirm={this.handleConfirm}
+        onCancel={onCancel}
+        onConfirm={onConfirm}
       >
         <Alert variant="warning"><strong>Warning:</strong> This action can not be reversed once you submit it.</Alert>
         <p> You have submitted a request to delete the following collection: </p>
         <br />
         <h1>
-          <strong>{this.props.collectionLabel}</strong>
+          <strong>{collectionLabel}</strong>
         </h1>
         <br />
         <p> Are you sure you want to permanently delete this collection? </p>
         <button
           className='button button--cancel button__animation--md button__arrow button__arrow--md button__animation button--secondary form-group__element--left'
           label="cancel"
-          onClick={this.handleCancel}
+          onClick={onCancel}
         >
             Cancel Request
         </button>
         <button
           className='button button__deletecollections button__animation--md button__arrow button__arrow--md button__animation button__arrow--white'
           label="confirm"
-          onClick={this.handleConfirm}
+          onClick={onConfirm}
         >
             Delete Collection
         </button>
       </DefaultModal>
 
-    );
-  }
-}
+);
 
 DeleteCollectionModal.propTypes = {
   collectionLabel: PropTypes.string,
