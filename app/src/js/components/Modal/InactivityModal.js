77114c09d70f40a084b8147935409a6d14250d1e
Richard Frausto <richard.frausto@suresecuresolutions.com>
2024-08-27 12:12:33 -0700

    finalized modal state

--- app/src/js/components/Modal/InactivityModal.js
+++ app/src/js/components/Modal/InactivityModal.js
@@ -1,37 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 // import { IDLE_TIMER_TOGGLE_MODAL, IDLE_TIMER_LAST_KEY_PRESS, IDLE_TIMER_LOGOUT_TIMER } from '../../actions/types';
 // import { idleTimerShowModal, idleTimerLastKeypress } from '../../actions/index';
+import get from 'lodash/get';
 import DefaultModal from './modal';
+// import OAuth from '../oauth';
+import { logout } from '../../actions';
 
 const InactivityModal = ({
   text = 'Session Timeout Warning',
+  children = `We have noticed that you have been inactive for a while.
+  We will close this session in 5 minutes. If you want to stay signed in, select ‘Continue Session’.`,
   showModal,
-  // className,
-  // error,
   onConfirm,
   onCloseModal,
-  onCancel
+  onCancel,
+  dispatch
 }) => {
-  // const [confirmModal, setConfirmModal] = useState(false);
-  // const [successModal, setSuccessModal] = useState(false);
   const [lastKeyPress, setLastKeyPress] = useState(Date.now());
   const [hasModal, setHasModal] = useState(true);
+  // const isLoggedIn = () => store.getState().api.authenticated;
 
   function handleConfirm() {
     setLastKeyPress(Date.now());
+    closeModal();
+  }
+
+  const handleLogout = useCallback(() => {
+    dispatch(logout()).then(() => {
+      if (get(window, 'location.reload')) {
+        window.location.reload();
+      }
+    });
+  }, [dispatch]);
+
+  function closeModal() { // the X botton
     if (hasModal) {
       setHasModal(false); // hide modal if user resumes activity
     }
-  }
-
-  function handleCancel() {
-    // closeModal
-  }
-
-  function handleLogout() {
-
   }
 
   // Effect to setup event listeners for keyboard activity
@@ -53,7 +60,7 @@ const InactivityModal = ({
   // Effect to handle showing the modal after 30 minutes of inactivity
   useEffect(() => {
     const checkInactivity = setInterval(() => {
-      if (Date.now() - lastKeyPress > 3000 && !hasModal) { // 1800000 ms = 30 minutes
+      if (Date.now() - lastKeyPress > 1800000 && !hasModal) { // 1800000 ms = 30 minutes
         setHasModal(true);
       }
     }, 60000); // Check every minute (60000 ms)
@@ -67,12 +74,13 @@ const InactivityModal = ({
       title = {text}
       className='IAModal'
       onCancel={handleLogout}
-      onCloseModal={handleCancel}
+      onCloseModal={closeModal}
       onConfirm={handleConfirm}
       showModal={hasModal}
       hasConfirmButton={hasModal}
       cancelButtonText='Cancel'
       confirmButtonText='Continue'
+      children = {children}
       />
     </div>
   );
@@ -90,6 +98,7 @@ InactivityModal.propTypes = {
   logoutTimer: PropTypes.object,
   title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   text: PropTypes.string,
+  children: PropTypes.string,
   className: PropTypes.string,
   cancelButtonText: PropTypes.string,
   confirmButtonText: PropTypes.string,
@@ -99,6 +108,7 @@ InactivityModal.propTypes = {
   onCancel: PropTypes.func,
   hasCancelButton: PropTypes.bool,
   hasConfirmButton: PropTypes.bool,
+  dispatch: PropTypes.func,
 };
 
 export default connect(mapStateToProps)(InactivityModal);
