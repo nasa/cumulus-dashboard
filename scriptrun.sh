9e2fe71f4459e33bf13f3253cfc20b63bb5170f0
Naga Nages <nnagavenkata@yahoo.com>
2024-10-15 11:58:07 -0400

    fixing linting

--- scriptrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# SIT shared dashboard
-export APIROOT=https://8o1cxx9bua.execute-api.us-east-1.amazonaws.com:8000/dev/
-export AUTH_METHOD=earthdata
-export AWS_REGION=us-east-1
-
-npm run build
-npm run serve
