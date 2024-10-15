5b4406866939218504e59e503311527e710212f5
Naga Nages <nnagavenkata@yahoo.com>
2024-10-15 11:57:55 -0400

    fixing linting issue

--- app/src/js/components/ReconciliationReports/list.js
+++ app/src/js/components/ReconciliationReports/list.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
