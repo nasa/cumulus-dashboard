d208aa16f782adf16819f3d89a0d92a7968e3e5c
Richard Frausto <richard.frausto@suresecuresolutions.com>
2024-10-21 13:50:33 -0700

    header unit test

--- test/components/header/header.js
+++ test/components/header/header.js
@@ -1,13 +1,13 @@
 'use strict';
 
 import test from 'ava';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+// import { shallow, configure } from 'enzyme';
 
 import { Header } from '../../../app/src/js/components/Header/header';
 
-configure({ adapter: new Adapter() });
+// configure({ adapter: new Adapter() });
 
 test('Header contains correct number of nav items and excludes PDRs and Logs', function (t) {
   const dispatch = () => {};
@@ -35,6 +35,32 @@ test('Header contains correct number of nav items and excludes PDRs and Logs', f
   t.is(navigation.contains('PDRs'), false);
   t.is(navigation.contains('Logs'), false);
 });
+
+// test('Cumulus API Version is not shown on the dashboard when not logged in', function (t) {
+//   const api = { };
+//   const apiVersion = {
+//     versionNumber: '1.11.0',
+//     warning: '',
+//     isCompatible: true
+//   };
+//   const cmrInfo = {
+//     cmrEnv: 'UAT',
+//     cmrProvider: 'CUMULUS',
+//     cmrOauthProvider: 'Launchpad'
+//   }
+ 
+ 
+//   const { container } = render(<Footer
+//       api={api}
+//       apiVersion={apiVersion}
+//       cmrInfo={cmrInfo}
+//     />
+//   )
+ 
+ 
+//   const versionInfo = container.querySelector('.version__info');
+//   t.falsy(versionInfo);
+//  });
 
 test('Logo path is "/cumulus-logo.png" when BUCKET is not specified', function (t) {
   const dispatch = () => {};
