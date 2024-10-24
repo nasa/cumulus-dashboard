742ff04a4829d2e84de76fc0a5982dc2a34201f5
Naga Nages <nnagavenkata@yahoo.com>
2024-10-24 13:25:21 -0400

    finished main package, partially completed oauth, and added changes to header

--- test/components/header/header.js
+++ test/components/header/header.js
@@ -1,17 +1,35 @@
 'use strict';
 
 import test from 'ava';
-// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { render, screen } from '@testing-library/react'
 import React from 'react';
-// import { shallow, configure } from 'enzyme';
 
 import { Header } from '../../../app/src/js/components/Header/header';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { requestMiddleware } from '../../../app/src/js/middleware/request';
 
-// configure({ adapter: new Adapter() });
+const middlewares = [requestMiddleware, thunk];
+const mockStore = configureMockStore(middlewares);
+const dispatch = () => {};
+const initialState = {
+  api: { authenticated: true },
+  locationQueryParams: { search: {} },
+  apiVersion : {
+    versionNumber: '1.11.0',
+    warning: '',
+    isCompatible: true
+  },
+  cmrInfo: {
+    cmrEnv: 'UAT',
+    cmrProvider: 'CUMULUS',
+    cmrOauthProvider: 'Launchpad'
+  }
+};
 
 test('Header contains correct number of nav items and excludes PDRs and Logs', function (t) {
-  const dispatch = () => {};
   const api = {
     authenticated: true
   }
@@ -22,13 +40,18 @@ test('Header contains correct number of nav items and excludes PDRs and Logs', f
     search: {}
   };
 
-  const { header } = render(
+  const someStore = mockStore(initialState);
+  const { container } = render(
+    <Provider store={someStore} >
+    <MemoryRouter>
     <Header
       dispatch={dispatch}
       api={api}
       location={location}
       locationQueryParams={locationQueryParams}
     />
+    </MemoryRouter>
+    </Provider>
   );
 
   const navigation = header.find('nav li');
@@ -36,6 +59,7 @@ test('Header contains correct number of nav items and excludes PDRs and Logs', f
   t.is(navigation.contains('PDRs'), false);
   t.is(navigation.contains('Logs'), false);
 });
+
 
 test('Logo path is "/cumulus-logo.png" when BUCKET is not specified', function (t) {
   const dispatch = () => {};
