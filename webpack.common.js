ee5f3991fa8b467d4f458bb671a448f7497fdab5
jennyhliu <jenny.h.liu@nasa.gov>
2024-11-21 19:08:50 -0500

    fix lint

--- webpack.common.js
+++ webpack.common.js
@@ -37,7 +37,7 @@ const CommonConfig = {
       stream: require.resolve('stream-browserify'),
       crypto: require.resolve('crypto-browserify'),
       util: require.resolve('util'),
-      vm: require.resolve("vm-browserify"),
+      vm: require.resolve('vm-browserify'),
     },
   },
   module: {
