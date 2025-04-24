9dda3576ce00004eca32361c0d32fb36a02da3cb
Nnaga1 <nnagavenkata@yahoo.com>
2025-04-24 15:50:06 -0400

    upgrading to v7.5.2 to fix vulns

---
+++ scriptrun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SIT shared dashboard
+export APIROOT=https://d84ilrgn1b7e1.cloudfront.net/
+export AUTH_METHOD=launchpad
+export AWS_REGION=us-east-1
+export DAAC_NAME=cumulus-std
+export KIBANAROOT=https://metrics.sit.earthdata.nasa.gov/s/metrics-cumulus
+export HIDE_PDR=false
+export STAGE=development
+export ENABLE_RECOVERY=true
+
+npm install
+npm run build
+npm run serve
