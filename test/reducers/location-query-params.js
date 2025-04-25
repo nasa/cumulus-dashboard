e264037731ec59f515d8e0e4d73cabf2117882bf
Nnaga1 <nnagavenkata@yahoo.com>
2025-04-24 20:58:06 -0400

    updated unit test

--- test/reducers/location-query-params.js
+++ test/reducers/location-query-params.js
@@ -2,21 +2,18 @@
 
 import test from 'ava';
 import cloneDeep from 'lodash/cloneDeep';
-import reducer, { initialState } from '../../app/src/js/reducers/location-query-params';
-import { LOCATION_CHANGE } from 'connected-react-router';
+import reducer, { initialState, updateSearchForPath} from '../../app/src/js/reducers/location-query-params';
 
-test('LOCATION_CHANGE reducer captures latest query params', (t) => {
+test('updateSearchForPath action captures latest query params', (t) => {
   const inputState = cloneDeep(initialState);
   const route = '/granules';
   const routeQueryParams = '?test=123';
-  inputState.prevLocationPathname = route;
   const expected = cloneDeep(initialState);
   expected.search = {
     [route]: routeQueryParams,
   };
-  expected.prevLocationPathname = route;
-
-  const action = { type: LOCATION_CHANGE, payload: { location: { pathname: route, search: routeQueryParams } } };
+            
+  const action = updateSearchForPath({ pathname: route, search: routeQueryParams });
   const actual = reducer(inputState, action);
-  t.deepEqual(expected, actual);
+  t.deepEqual(actual, expected);
 });
