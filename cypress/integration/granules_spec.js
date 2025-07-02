2a471740c5e684c194979ab8cbefe28ae8077e0a
Allan Yu <acyu@ndc.nasa.gov>
2025-07-01 21:30:50 -0700

    add prefix and infix integration test for granules and collections

--- cypress/integration/granules_spec.js
+++ cypress/integration/granules_spec.js
@@ -301,7 +301,7 @@ describe('Dashboard Granules Page', () => {
     });
 
     it('Should update URL and table when search filter is changed.', () => {
-      const prefix = 'MOD09GQ.A802';
+      const prefix = 'test_';
       cy.visit('/granules');
       cy.get('.search').as('search');
       cy.get('@search').click().type(prefix);
@@ -309,6 +309,38 @@ describe('Dashboard Granules Page', () => {
       cy.get('.table .tbody .tr').should('have.length', 1);
       cy.get('.table .tbody .tr').eq(0).children('.td').eq(2)
         .contains(prefix);
+    });
+
+    it('Should update results when toggling Search By Prefix', () => {
+      const prefix = 'MOD09GQ.A241';
+      const infix = 'A153';
+      cy.visit('/granules');
+      cy.get('#chk_isprefixsearch').should('be.checked');
+      cy.get('.search').as('search');
+      cy.get('@search').click().type(prefix);
+      cy.get('.table .tbody .tr').should('have.length', 1);
+      cy.get('@search').click().type(infix);
+      cy.get('.table .tbody .tr').should('have.length', 0);
+      cy.get('#chk_isprefixsearch').click().should('not.be.checked');
+      cy.get('.table .tbody .tr').should('have.length.at.least', 1);
+      cy.get('.table .tbody .tr').eq(0).children('.td').eq(2)
+        .contains(infix);
+    });
+
+    it('Should have Search by prefix input in granule status list view and update results when toggling Search By Prefix', () => {
+      const prefix = 'test_';
+      const infix = 'A201';
+      cy.visit('/granules/completed');
+      cy.get('#chk_isprefixsearch').should('be.checked');
+      cy.get('.search').as('search');
+      cy.get('@search').click().type(prefix);
+      cy.get('.table .tbody .tr').should('have.length', 1);
+      cy.get('@search').click().type(infix);
+      cy.get('.table .tbody .tr').should('have.length', 0);
+      cy.get('#chk_isprefixsearch').click().should('not.be.checked');
+      cy.get('.table .tbody .tr').should('have.length.at.least', 1);
+      cy.get('.table .tbody .tr').eq(0).children('.td').eq(2)
+        .contains(infix);
     });
 
     it('Should show Search and Dropdown filters in URL.', () => {
