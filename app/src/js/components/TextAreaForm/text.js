8dbe02456532bc98e54323babdc595aa54ea4320
gacondino <gaerrielle105@gmail.com>
2025-07-30 13:34:13 -0400

    first commit

--- app/src/js/components/TextAreaForm/text.js
+++ app/src/js/components/TextAreaForm/text.js
@@ -1,46 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class TextForm extends React.Component {
-  constructor () {
-    super();
-    this.onChange = this.onChange.bind(this);
-  }
+const TextForm = ({
+  label,
+  value,
+  id,
+  error,
+  onChange,
+  type,
+  className
+}) => {
+  const handleChange = (e) => {
+    onChange(id, e.target.value);
+  };
 
-  onChange (e) {
-    this.props.onChange(this.props.id, e.target.value);
-  }
-
-  render () {
-    let {
-      label,
-      value,
-      id,
-      error,
-      type,
-      className,
-      ...rest
-    } = this.props;
-
-    type = type || 'text';
-
-    return (
-      <div className={`form__text${error ? ' form__error--wrapper' : ''}`}>
-        <label htmlFor={id}>{label}</label>
-        {error && <span className='form__error'>{error}</span>}
-        <input
-          {...rest}
-          id={id}
-          type={type}
-          value={value}
-          className={className}
-          onChange={this.onChange}
-        />
-      </div>
-    );
-  }
-}
-
+  return (
+    <div className={`form__text${error ? ' form__error--wrapper' : ''}`}>
+      <label htmlFor={id}>{label}</label>
+      {error && <span className='form__error'>{error}</span>}
+      <input
+        id={id}
+        type={type || 'text'}
+        value={value}
+        className={className}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 TextForm.propTypes = {
   label: PropTypes.any,
   value: PropTypes.oneOfType([
