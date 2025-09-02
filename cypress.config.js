6fcc713cb9e1e19cb1f0a1288e2de8c18349825a
Austin Wisdom <austin.j.wisdom@nasa.gov>
2025-09-02 12:33:53 -0400

    migrating Cypress specs and config files to Cypress v10

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  env: {
+    APIROOT: 'http://localhost:5001',
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  viewportWidth: 1300,
+  viewportHeight: 1000,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+})
