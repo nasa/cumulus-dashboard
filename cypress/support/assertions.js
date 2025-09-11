bb2e4b83f95ec4ac927c0c6b28affe37a63b48c5
Austin Wisdom <austin.j.wisdom@nasa.gov>
2025-09-11 10:28:39 -0400

    restoring commands and assertions back to original

--- cypress/support/assertions.js
+++ cypress/support/assertions.js
@@ -13,15 +13,19 @@ exports.shouldBeRedirectedToLogin = () => {
 };
 
 exports.shouldHaveNoToken = () => {
-  cy.wrap(Cypress.env('authToken')).should('be.null');
-  
-  // Assert local storage is null
-  cy.window().its('localStorage').invoke('getItem', 'auth-token').should('be.null');
+  cy.window().its('appStore').then((store) => {
+    expect(store.getState().api.tokens.token).to.equal(null);
+    cy.window().its('localStorage').invoke('getItem', 'auth-token').then((token) => {
+      expect(token).to.equal(null);
+    });
+  });
 };
 
 exports.shouldHaveDeletedToken = () => {
-  cy.wrap(Cypress.env('authToken')).should('eq', '');
-
-  // Assert local storage is an empty string
-  cy.window().its('localStorage').invoke('getItem', 'auth-token').should('eq', '');
+  cy.window().its('appStore').then((store) => {
+    expect(store.getState().api.tokens.token).to.equal(null);
+    cy.window().its('localStorage').invoke('getItem', 'auth-token').then((token) => {
+      expect(token).to.eq('');
+    });
+  });
 };
