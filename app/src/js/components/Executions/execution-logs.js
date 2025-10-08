afdbb11e8b7e1f119b73b52c19c6c789684cd16b
Austin Wisdom <austin.j.wisdom@nasa.gov>
2025-10-08 15:04:38 -0400

    converted execution logs class component into a functional component

--- app/src/js/components/Executions/execution-logs.js
+++ app/src/js/components/Executions/execution-logs.js
@@ -1,102 +1,81 @@
-import React from 'react';
+import React, { useEffect, useMemo, useCallback } from 'react';
 import { Helmet } from 'react-helmet';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
 import { getExecutionLogs } from '../../actions';
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
-
 import ErrorReport from '../Errors/report';
-import { historyPushWithQueryParams } from '../../utils/url-helper';
 
-class ExecutionLogs extends React.Component {
-  constructor(props) {
-    super(props);
-    this.navigateBack = this.navigateBack.bind(this);
-    this.errors = this.errors.bind(this);
-    this.executionName = this.getExecutionName();
-  }
+const ExecutionLogs = () => {
+  const { executionArn } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(getExecutionLogs(this.executionName));
-  }
+  const executionName = useMemo(
+    () => (executionArn ? executionArn.split(':').pop() : ''),
+    [executionArn]
+  );
 
-  getExecutionName() {
-    const { executionArn } = this.props.match.params;
-    return executionArn.split(':').pop();
-  }
+  const executionLogs = useSelector((state) => state.executionLogs);
 
-  navigateBack() {
-    historyPushWithQueryParams('/executions');
-  }
+  useEffect(() => {
+    if (executionName) {
+      dispatch(getExecutionLogs(executionName));
+    }
+  }, [dispatch, executionName]);
 
-  errors() {
-    return [].filter(Boolean);
-  }
+  const navigateBack = useCallback(() => {
+    history.push('/executions');
+  }, [history]);
 
-  render() {
-    const breadcrumbConfig = [
-      {
-        label: 'Dashboard Home',
-        href: '/',
-      },
-      {
-        label: 'Executions',
-        href: '/executions'
-      },
-      {
-        label: 'Execution Logs',
-        active: true
-      }
-    ];
+  const errors = () => [];
 
-    const { executionLogs } = this.props;
-    if (!executionLogs.results) return null;
-    const errors = this.errors();
+  const breadcrumbConfig = [
+    {
+      label: 'Dashboard Home',
+      href: '/',
+    },
+    {
+      label: 'Executions',
+      href: '/executions',
+    },
+    {
+      label: 'Execution Logs',
+      active: true,
+    },
+  ];
 
-    return (
-      <div className='page__component'>
-        <Helmet>
-          <title> Execution Logs </title>
-        </Helmet>
-        <section className="page__section page__section__controls">
-          <Breadcrumbs config={breadcrumbConfig} />
-        </section>
-        <section className='page__section page__section__header-wrapper'>
-          <h1 className='heading--large heading--shared-content with-description'>
-            Logs for Execution {this.executionName}
-          </h1>
+  if (!executionLogs.results) return null;
 
+  return (
+    <div className='page__component'>
+      <Helmet>
+        <title> Execution Logs </title>
+      </Helmet>
+      <section className="page__section page__section__controls">
+        <Breadcrumbs config={breadcrumbConfig} />
+      </section>
+      <section className='page__section page__section__header-wrapper'>
+        <h1 className='heading--large heading--shared-content with-description'>
+          Logs for Execution {executionName}
+        </h1>
           {errors.length ? <ErrorReport report={errors} /> : null}
-        </section>
+      </section>
 
-        <section className='page__section' style={{ display: 'inline-block', verticalAlign: 'top' }}>
-
-          <div className='status--process'>
-            <h2>Execution Details:</h2>
-            <pre>{JSON.stringify(executionLogs.details, null, 2)}</pre><br />
-          </div>
-
-          <div className='status--process'>
-            <h2>Execution Logs:</h2>
-            <pre>{JSON.stringify(executionLogs.results, null, 2)}</pre><br />
-          </div>
-
-        </section>
-      </div>
-    );
-  }
-}
-
-ExecutionLogs.propTypes = {
-  executionLogs: PropTypes.object,
-  match: PropTypes.object,
-  dispatch: PropTypes.func,
+      <section className='page__section' style={{ display: 'inline-block', verticalAlign: 'top' }}>
+        <div className='status--process'>
+          <h2>Execution Details:</h2>
+          <pre>{JSON.stringify(executionLogs.details, null, 2)}</pre><br />
+        </div>
+        <div className='status--process'>
+          <h2>Execution Logs:</h2>
+          <pre>{JSON.stringify(executionLogs.results, null, 2)}</pre><br />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 ExecutionLogs.displayName = 'Execution Logs';
 
-export default withRouter(connect((state) => ({
-  executionLogs: state.executionLogs
-}))(ExecutionLogs));
+export default ExecutionLogs;
