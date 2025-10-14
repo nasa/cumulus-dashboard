39c8c724808d789326cb463b9448565b27dcf194
Austin Wisdom <austin.j.wisdom@nasa.gov>
2025-10-14 17:50:38 -0400

    convert granule progress class component to functional component

--- app/src/js/components/Granules/progress.js
+++ app/src/js/components/Granules/progress.js
@@ -4,24 +4,18 @@ import { get } from 'object-path';
 import { tally } from '../../utils/format';
 import { strings } from '../locale';
 
-// defines the order in which the granules meta bar appears
 const granuleMeta = [
   ['running', 'Running'],
   ['completed', 'Completed'],
   ['failed', 'Failed']
 ];
-class Progress extends React.Component {
-  constructor () {
-    super();
-    this.getItem = this.getItem.bind(this);
-    this.tallyDisplay = this.tallyDisplay.bind(this);
-  }
 
-  getItem (key) {
-    return this.props.granules.find((count) => count.key === key);
-  }
+const Progress = ({ granules }) => {
+  const getItem = (key) => {
+    return granules.find((count) => count.key === key);
+  };
 
-  tallyDisplay (type, item) {
+  const tallyDisplay = (type, item) => {
     if (type[1] === 'Failed' && item > 0) {
       if (item > 99) {
         return (
@@ -39,26 +33,26 @@ class Progress extends React.Component {
     }
   }
 
-  render () {
-    return (
-      <ul className='timeline--processing--overall'>
-        {granuleMeta.map((d) => {
-          const item = Array.isArray(d[0])
-            ? d[0].map(this.getItem).reduce((a, b) => a + get(b, 'count', 0), 0)
-            : get(this.getItem(d[0]), 'count', 0);
+  return (
+    <ul className='timeline--processing--overall'>
+      {granuleMeta.map((d) => {
+        const item = Array.isArray(d[0])
+          ? d[0].map(getItem).reduce((a, b) => a + get(b, 'count', 0), 0)
+          : get(getItem(d[0]), 'count', 0);
 
-          return (
-            <li key={d[0]} className={`timeline--processing--${d[0]}`}>
-              {this.tallyDisplay(d, tally(item))}
-              {strings.granules} {d[1]}
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
+        return (
+          <li key={d[0]} className={`timeline--processing--${d[0]}`}>
+            {tallyDisplay(d, tally(item))}
+            {strings.granules} {d[1]}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-Progress.propTypes = { granules: PropTypes.array };
+Progress.propTypes = {
+  granules: PropTypes.array
+};
 
 export default Progress;
