b26a3d71cb9631072dcf77c74a8f582187ed4d1a
Austin Wisdom <austinwisdom0816@gmail.com>
2025-11-18 17:06:30 -0500

    added cypress tests for output column

--- cypress/e2e/operations.cy.js
+++ cypress/e2e/operations.cy.js
@@ -30,7 +30,7 @@ describe('Operations page', () => {
       cy.visit('/');
       cy.get('nav').contains('Operations').click();
       cy.url().should('include', '/operations');
-      cy.get('.table .tbody .tr').should('have.length', 7);
+      cy.get('.table .tbody .tr').should('have.length', 8);
       cy.contains('.table .tr', testAsyncId)
         .within(() => {
           cy.contains(testDescription);
@@ -47,6 +47,43 @@ describe('Operations page', () => {
       cy.get('@search').click().type(search);
       cy.url().should('include', `search=${search}`);
       cy.get('.table .tbody .tr').should('have.length', 1);
+    });
+
+    it('should handle null output gracefully for RUNNING operations', () => {
+      cy.visit('/operations');
+      const runningId = '58397019-81fb-464a-bc76-0ad0490fdeec';
+      cy.contains('.table .tr', runningId)
+        .within(() => {
+          cy.get('p').should('not.exist');
+          cy.get('.error__report').should('not.exist');
+        });
+    });
+
+    it('should display JSON output for SUCCEEDED operations', () => {
+      cy.visit('/operations');
+      cy.contains('.table .tr', testAsyncId)
+        .within(() => {
+          cy.get('p').should('exist');
+          cy.get('.error__report').should('not.exist');
+        });
+    });
+
+    it('should display ErrorReport for TASK_FAILED operations', () => {
+      cy.visit('/operations');
+      const taskFailedId = 'c81fa7ad-d83b-4ed0-bfbf-fca16a329836';
+      cy.contains('.table .tr', taskFailedId)
+        .within(() => {
+          cy.get('.error__report').should('exist');
+        });
+    });
+
+    it('should display ErrorReport for RUNNER_FAILED operations', () => {
+      cy.visit('/operations');
+      const runnerFailedId = '1783f739-742d-4095-9283-3f65f37b2a7e';
+      cy.contains('.table .tr', runnerFailedId)
+        .within(() => {
+          cy.get('.error__report').should('exist');
+        });
     });
   });
 });
