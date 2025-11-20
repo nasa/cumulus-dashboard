c1642156d0a1557a80142d2007bb965259aa8e70
Austin Wisdom <austinwisdom0816@gmail.com>
2025-11-20 10:46:46 -0500

    adding route for operation-status page

--- app/src/js/paths/operations.js
+++ app/src/js/paths/operations.js
@@ -1,9 +1,12 @@
+import { encode } from '../utils/browser';
+
 const routes = [
   ['Overview', null]
 ];
 
 const singleRoutes = [
-  ['Back to Operations', null, 'sidebar__nav--back']
+  ['Back to Operations', null, 'sidebar__nav--back'],
+  ['Details', 'operation/:operationId']
 ];
 
 const empty = [['', '']];
@@ -12,8 +15,13 @@ const handler = {
   base: 'operations',
   heading: 'Operations',
   routes: (currentRoute, params) => {
-    if (currentRoute.includes('operations/operation')) {
-      return singleRoutes;
+    if (currentRoute.includes('operations/operation/')) {
+      return singleRoutes.map((d) => {
+        if (!d[1] || !d[1].includes(':operationId')) { return d; }
+        const copy = d.slice();
+        copy[1] = encode(copy[1].replace(':operationId', params.operationId));
+        return copy;
+      });
     }
 
     if (currentRoute.slice(0, 12) !== '/operations') {
