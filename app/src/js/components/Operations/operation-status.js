aad822ee1f940a758100bdc6c8e520e1c14e12f3
Austin Wisdom <austinwisdom0816@gmail.com>
2025-11-21 09:42:38 -0500

    fixed linter errors

--- app/src/js/components/Operations/operation-status.js
+++ app/src/js/components/Operations/operation-status.js
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getOperation } from '../../actions';
-import { displayCase, parseJson } from '../../utils/format';
+import { displayCase } from '../../utils/format';
 import { window } from '../../utils/browser';
 import ErrorReport from '../Errors/report';
 import Metadata from '../Table/Metadata';
@@ -30,96 +30,94 @@ const metaAccessors = ({
   operation,
   showOutputModal,
   toggleModal
-}) => {
-  return [
-    {
-      label: 'ID',
-      property: 'id'
-    },
-    {
-      label: 'Status',
-      property: 'status',
-      accessor: (value) => displayCase(value)
-    },
-    {
-      label: 'Operation Type',
-      property: 'operationType'
-    },
-    {
-      label: 'Description',
-      property: 'description'
-    },
-    {
-      label: 'Task ARN',
-      property: 'taskArn'
-    },
-    {
-      label: 'Output',
-      property: 'output',
-      accessor: (d) => {
-        if (d) {
-          const jsonData = typeof Blob !== 'undefined' ? new Blob([d], { type: 'text/json' }) : null;
-          const downloadUrl = typeof window.URL.createObjectURL === 'function' ? window.URL.createObjectURL(jsonData) : '';
+}) => [
+  {
+    label: 'ID',
+    property: 'id'
+  },
+  {
+    label: 'Status',
+    property: 'status',
+    accessor: (value) => displayCase(value)
+  },
+  {
+    label: 'Operation Type',
+    property: 'operationType'
+  },
+  {
+    label: 'Description',
+    property: 'description'
+  },
+  {
+    label: 'Task ARN',
+    property: 'taskArn'
+  },
+  {
+    label: 'Output',
+    property: 'output',
+    accessor: (d) => {
+      if (d) {
+        const jsonData = typeof Blob !== 'undefined' ? new Blob([d], { type: 'text/json' }) : null;
+        const downloadUrl = typeof window.URL.createObjectURL === 'function' ? window.URL.createObjectURL(jsonData) : '';
 
-          let formattedOutput = d;
-          try {
-            // If it's a JSON string, parse and pretty print it
-            // If it's already an object, stringify it
-            // If it's a simple string, leave as is
-            if (typeof d === 'string') {
-              const parsed = JSON.parse(d);
-              formattedOutput = JSON.stringify(parsed, null, 2);
-            } else if (typeof d === 'object') {
-              formattedOutput = JSON.stringify(d, null, 2);
-            }
-          } catch (e) {
-            // Fallback to original value
+        let formattedOutput = d;
+        try {
+          // If it's a JSON string, parse and pretty print it
+          // If it's already an object, stringify it
+          // If it's a simple string, leave as is
+          if (typeof d === 'string') {
+            const parsed = JSON.parse(d);
+            formattedOutput = JSON.stringify(parsed, null, 2);
+          } else if (typeof d === 'object') {
+            formattedOutput = JSON.stringify(d, null, 2);
           }
-
-          return (
-            <>
-              <button
-                onClick={() => toggleModal('output')}
-                className="button button--small button--no-left-padding"
-              >
-                Show Output
-              </button>
-              <DefaultModal
-                showModal={showOutputModal}
-                title={
-                  <>
-                    <span>Operation Output</span>
-                    <a
-                      className="button button--small button--download button--green form-group__element--right"
-                      id="download_link"
-                      download="output.json"
-                      href={downloadUrl}
-                    >
-                      Download File
-                    </a>
-                  </>
-                }
-                onCloseModal={() => toggleModal('output')}
-                hasConfirmButton={false}
-                cancelButtonClass="button--close"
-                cancelButtonText="Close"
-                className="operation__modal"
-              >
-                <pre>{formattedOutput}</pre>
-              </DefaultModal>
-            </>
-          );
+        } catch (e) {
+          // Fallback to original value
         }
-        return 'N/A';
+
+        return (
+          <>
+            <button
+              onClick={() => toggleModal('output')}
+              className="button button--small button--no-left-padding"
+            >
+              Show Output
+            </button>
+            <DefaultModal
+              showModal={showOutputModal}
+              title={
+                <>
+                  <span>Operation Output</span>
+                  <a
+                    className="button button--small button--download button--green form-group__element--right"
+                    id="download_link"
+                    download="output.json"
+                    href={downloadUrl}
+                  >
+                    Download File
+                  </a>
+                </>
+              }
+              onCloseModal={() => toggleModal('output')}
+              hasConfirmButton={false}
+              cancelButtonClass="button--close"
+              cancelButtonText="Close"
+              className="operation__modal"
+            >
+              <pre>{formattedOutput}</pre>
+            </DefaultModal>
+          </>
+        );
       }
+      return 'N/A';
     }
-  ];
-};
+  }
+];
 
 const OperationStatus = () => {
   const { operationId } = useParams();
   const dispatch = useDispatch();
-  const { data: operation, error, inflight } = useSelector(state => state.operationStatus) || {};
+  const { data: operation, error, inflight } = useSelector((state) => state.operationStatus) || {};
 
   const [showOutputModal, setShowOutputModal] = useState(false);
 
