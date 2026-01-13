44c0a473edb30517a8b9b0a1cb32cecee62970a5
Yonggang Liu <ygliuvt@gmail.com>
2026-01-13 14:12:51 -0500

    CUMULUS-4484: Update ava config.

--- ava-conditional.config.cjs
+++ ava-conditional.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
     HIDE_PDR: 'false',
     KIBANAROOT: 'https://fake.com/linktokibana',
     AUTH_METHOD: 'launchpad',
-    INITIAL_DATE_RANGE_IN_DAYS: '',
+    INITIAL_DATE_RANGE_IN_DAYS: 'All',
     BUCKET: 'https://example.com/bucket',
     STAGE: 'production',
     DAAC_NAME: 'Cumulus_Daac',
