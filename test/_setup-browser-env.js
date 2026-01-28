51a6dcba9b1e1ff6469792181af08282a3ddd78b
wisdomaj <austinwisdom0816@gmail.com>
2026-01-28 17:51:09 -0500

    adding mocks for unit tests

--- test/_setup-browser-env.js
+++ test/_setup-browser-env.js
@@ -1,1 +1,11 @@
 require('browser-env')({ url: 'https://example.com' });
+
+// Initialize test-only configuration properties
+// These can be set by individual tests as needed for mocking
+const config = require('../app/src/js/config');
+
+// Set to a timestamp (in seconds) to mock token expiration for testing
+config.mockTokenExpiration = null;
+
+// Set to a timestamp (in ms) to mock session start for testing
+config.mockSessionStart = null;
