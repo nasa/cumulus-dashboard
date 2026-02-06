ec7397cd583ebb038e9620550b6cde36673b61fe
Allan Yu <allan.c.yu@jpl.nasa.gov>
2026-02-06 13:17:19 -0800

    Update error cause 'None' checking into using regex

--- app/src/js/utils/table-config/granules.js
+++ app/src/js/utils/table-config/granules.js
@@ -5,7 +5,6 @@ import Collapsible from 'react-collapsible';
 import { Link } from 'react-router-dom';
 import noop from 'lodash/noop';
 import isEmpty from 'lodash/isEmpty';
-import includes from 'lodash/includes';
 import {
   seconds,
   bool,
@@ -71,7 +70,7 @@ export const tableColumns = [
     Header: 'Error Type',
     accessor: (row) => get(row, 'error.Error', nullValue),
     Cell: ({ row: { original: { error } } }) => (
-      (isEmpty(error) || includes(['None', '"None"'], error.Cause)) ? nullValue : error.Error
+      (isEmpty(error) || /^("?)(None)\1$/.test(error.Cause)) ? nullValue : error.Error
     ),
     id: 'errorType',
     width: 100
@@ -81,7 +80,7 @@ export const tableColumns = [
     accessor: (row) => get(row, 'error.Cause', nullValue),
     id: 'error',
     Cell: ({ row: { original } }) => ( // eslint-disable-line react/prop-types
-      (isEmpty(get(original, ['error'])) || includes(['None', '"None"'], get(original, ['error', 'Cause']))) ? nullValue : <ErrorReport report={get(original, 'error', nullValue)} truncate={true} disableScroll={true} />
+      (isEmpty(get(original, ['error'])) || /^("?)(None)\1$/.test(get(original, ['error', 'Cause']))) ? nullValue : <ErrorReport report={get(original, 'error', nullValue)} truncate={true} disableScroll={true} />
     ),
     disableSortBy: true,
     width: 175
